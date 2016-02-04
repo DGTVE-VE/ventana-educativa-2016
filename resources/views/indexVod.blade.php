ed778e7be132addcae7563311b04b9a307e7ca7d
EDA1 <EDA1>
2016-02-04 11:15:10 -0600

    Corregido navegación en menú Ventana Educativa

--- resources/views/indexVod.blade.php
+++ resources/views/indexVod.blade.php
@@ -2,15 +2,8 @@
 VOD
 @stop
 @extends('home')
-<<<<<<< HEAD
-
-@section('cuerpo')
-<div><h1>VOD</h1></div>
-@stop
-=======
 @section('cuerpo')
 <div>
     <h1>VOD</h1>
 </div>
 @endsection
->>>>>>> c80a304b4371c557071370783fce1dd2a235f96a
