df3395e8a79d5160e4cc0d1e7ace5f2a595dd0aa
Sonia Martinez <soniamartinezctr@gmail.com>
2016-02-12 12:46:16 -0600

    cambio en función de mapa

--- resources/views/red/integrantes.php
+++ resources/views/red/integrantes.php
@@ -5,17 +5,17 @@
         </div>
     </div>
     <div class="col-md-12">
-        <img name="imagenMapa_n"  src="imagenes/red/integrantes/gris.png"  border="5px" id="imagenMapa" class="mapaCentroAmerica img-responsive" usemap="#mapa_n" alt="" />
-        <map name="mapa_n" id="mapa">         
-            <area alt="Colombia" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/colombia.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="331,279,347,261,347,226,377,187,394,230,434,238,431,274,414,279,413,321,381,380,318,330"/>
-            <area alt="Costa Rica" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/costarica.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="265,192,290,182,308,208,307,223,291,216"/>
-            <area alt="Dominicana" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/dominicana.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="379,130,389,111,414,114,433,130"/>
-            <area alt="El Salvador" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/salvador.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="237,173,243,155,254,183"/>
-            <area alt="Guatemala" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/guatemala.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="214,162,240,128,249,143,237,173"/>
-            <area alt="Honduras" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/honduras.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="242,156,247,142,290,146,253,183"/>
-            <area alt="México" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/mexico.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="126,131,187,124,225,112,227,94,262,100,216,162,160,147"/>
-            <area alt="Nicaragua" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/nicaragua.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="254,182,292,145,291,181,267,194"/>
-            <area alt="Panama" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/panama.png';" onmouseout="saleMouse();" href="#" shape="poly" coords="309,223,311,207,346,201,348,212,360,212,348,228,326,231"/>
+        <img name="imagenMapa_n"  src="imagenes/red/integrantes/gris.png"  border="5px" id="imagenMapa" class="mapaCentroAmerica img-responsive" usemap="#mapa" alt="" />
+        <map name="mapa" id="mapa">         
+            <area alt="Colombia" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/colombia.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="331,279,347,261,347,226,377,187,394,230,434,238,431,274,414,279,413,321,381,380,318,330"/>
+            <area alt="Costa Rica" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/costarica.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="265,192,290,182,308,208,307,223,291,216"/>
+            <area alt="Dominicana" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/dominicana.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="379,130,389,111,414,114,433,130"/>
+            <area alt="El Salvador" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/salvador.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="237,173,243,155,254,183"/>
+            <area alt="Guatemala" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/guatemala.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="214,162,240,128,249,143,237,173"/>
+            <area alt="Honduras" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/honduras.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="242,156,247,142,290,146,253,183"/>
+            <area alt="México" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/mexico.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="126,131,187,124,225,112,227,94,262,100,216,162,160,147"/>
+            <area alt="Nicaragua" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/nicaragua.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="254,182,292,145,291,181,267,194"/>
+            <area alt="Panama" onmouseover="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/panama.png';" onmouseout="document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';" href="#" shape="poly" coords="309,223,311,207,346,201,348,212,360,212,348,228,326,231"/>
         </map>
     </div>
 </div>
@@ -23,7 +23,7 @@
 <script>
     function saleMouse()
     {
-        document.getElementById("imagenMapa").src = "imagenes/red/integrantes/gris.png";
+//        document.getElementById('imagenMapa').src = 'imagenes/red/integrantes/gris.png';
     }
 
 </script>
