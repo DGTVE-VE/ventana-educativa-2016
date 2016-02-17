1c69558f4b30bb2cf4ab1db551ace9ce79d4a506
EDA1 <EDA1>
2016-02-17 11:55:14 -0600

    Cambio en texto slider en sección retos regionales

--- resources/views/red/retosRegionales.php
+++ resources/views/red/retosRegionales.php
@@ -5,9 +5,9 @@
 				</div>
 				<div class="col-md-4 col-md-offset-1">
 					<h3>Retos Regionales</h3>
-					<div id="carouselRetos" class="carousel slide" data-ride="carousel" style="width:450px; background-color:#E5E5E5;">
+					<div id="carouselRetos" class="carousel slide" data-ride="carousel"> <!-- -->
 					  <!-- Indicators -->
-					  <ol class="carousel-indicators">
+					  <ol class="carousel-indicators" style="background-color:#E5E5E5;">
 						<li data-target="#carouselRetos" data-slide-to="0" class="active"></li>
 						<li data-target="#carouselRetos" data-slide-to="1"></li>
 						<li data-target="#carouselRetos" data-slide-to="2"></li>
@@ -20,47 +20,47 @@
 					  <!-- Wrapper for slides -->
 					  <div class="carousel-inner" role="listbox">
 						<div class="item active">
-						  <img src="imagenes/red/iconosRetos/1.png" style="width:450px; height:450px;" alt="...">
-						  <div class="carousel-caption textoSombra"> <!--style="position:relative; top:0px; background-color:#D3D3D3; color:black;"-->
+						  <img src="imagenes/red/iconosRetos/1.png" class="imgIconosRetos" alt="...">
+						  <div class="txtCarouselAbajo"> <!--carousel-caption background-color:#D3D3D3;-->
 							<p>Ser un espacio mesoamericano que permita compartir las experiencias, conocimientos y perspectivas entre los miembros de la Red.</p>
 						  </div>
 						</div>
 						<div class="item">
-						  <img src="imagenes/red/iconosRetos/2.png" style="width:450px; height:450px;" alt="...">
-						  <div class="carousel-caption textoSombra">
+						  <img src="imagenes/red/iconosRetos/2.png" class="imgIconosRetos" alt="...">
+						  <div class="txtCarouselAbajo">
 							<p>Reconocer el poder de las tecnologías de la información y comunicación (TIC), para promover el intercambio de conocimientos, 
 							la cooperación técnica y el impulso a la investigación educativa, en favor de la región mesoamericana.</p>
 						  </div>
 						</div>
 						<div class="item">
-						  <img src="imagenes/red/iconosRetos/3.png" style="width:450px; height:450px;" alt="...">
-						  <div class="carousel-caption textoSombra">
+						  <img src="imagenes/red/iconosRetos/3.png" class="imgIconosRetos" alt="...">
+						  <div class="txtCarouselAbajo">
 							<p>Proporcionar a la comunidad educativa -estudiantes, docentes y  padres de familia- la posibilidad de participar 
 							activamente en la creación de contenidos educativos, con la finalidad de consolidar una ciudadanía digital.</p>
 						  </div>
 						</div>
 						<div class="item">
-						  <img src="imagenes/red/iconosRetos/4.png" style="width:450px; height:450px;" alt="...">
-						  <div class="carousel-caption textoSombra">
+						  <img src="imagenes/red/iconosRetos/4.png" class="imgIconosRetos" alt="...">
+						  <div class="txtCarouselAbajo">
 							<p>Promover estrategias educativas de alcance mesoamericano para el desarrollo, integración y adopción de las tecnologías 
 							de la información y comunicación (TIC).</p>
 						  </div>
 						</div>
 						<div class="item">
-						  <img src="imagenes/red/iconosRetos/5.png" style="width:450px; height:450px;" alt="...">
-						  <div class="carousel-caption textoSombra">
+						  <img src="imagenes/red/iconosRetos/5.png" class="imgIconosRetos" alt="...">
+						  <div class="txtCarouselAbajo">
 							<p>Generar investigación aplicada a las deficiencias académicas comunes en las áreas de matemáticas y español.</p>
 						  </div>
 						</div>
 						<div class="item">
-						  <img src="imagenes/red/iconosRetos/6.png" style="width:450px; height:450px;" alt="...">
-						  <div class="carousel-caption textoSombra">
+						  <img src="imagenes/red/iconosRetos/6.png" class="imgIconosRetos" alt="...">
+						  <div class="txtCarouselAbajo">
 							<p>Crear instrumentos de evaluación para determinar el impacto del uso de las TIC en educación básica.</p>
 						  </div>
 						</div>
 						<div class="item">
-						  <img src="imagenes/red/iconosRetos/7.png" style="width:450px; height:450px;" alt="...">
-						  <div class="carousel-caption textoSombra">
+						  <img src="imagenes/red/iconosRetos/7.png" class="imgIconosRetos" alt="...">
+						  <div class="txtCarouselAbajo">
 							<p>Desarrollar modelos de pedagogía digital que orienten la creación, gestión y categorización de los contenidos 
 							educativos que se difundan en la plataforma digital Ventana Educativa.</p>
 						  </div>
