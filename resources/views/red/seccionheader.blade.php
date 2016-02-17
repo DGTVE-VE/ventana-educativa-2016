8cb17cd676a9baa9e0541f0409f3e8d8b4532d9c
EDA1 <EDA1>
2016-02-17 15:54:31 -0600

    Modificada sección header. En opción Inicio del menu de navegación agregada la liga para que dirija a la página de red mesoamericana

--- resources/views/red/seccionheader.blade.php
+++ resources/views/red/seccionheader.blade.php
@@ -11,7 +11,7 @@
 
     <div class="collapse navbar-collapse js-navbar-collapse">
         <ul class="nav navbar-nav navbar-right text-uppercase bajaHorizontal">
-		    <li><a href="#">INICIO</a></li>
+		    <li><a href="indexRed">INICIO</a></li>
                 <!-- <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Objetivos b class="caret"></b</a>
                 <ul class="dropdown-menu">
