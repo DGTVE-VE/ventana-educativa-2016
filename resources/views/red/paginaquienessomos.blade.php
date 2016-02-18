bea5dd9b2b329e86f1c2ec326f0f3e38a342e9f1
grisvelazquez <grisvelazquez.v@gmail.com>
2016-02-18 12:43:40 -0600

    Se reemplazo la imagen de  "LogoNegativo" del pie de página con un tamaño de 120 X 120

--- resources/views/red/paginaquienessomos.blade.php
+++ resources/views/red/paginaquienessomos.blade.php
@@ -7,14 +7,15 @@
 @endsection
 @section('cuerpo')
 	
-        <div class="container" style="margin-top: 80px;">
-            <div class="row">
-                <div class="col-md-12 thumbnail item square animated bounceIn">
+        <div class="container" style="margin-top: 80px; ">
+            <div class="row" style="background:url(ventanaquienessomos2.jpg) no-repeat; background-size: cover;">
+                <div class="col-md-12 thumbnail item square animated bounceIn" >
                         <!--<img src="http://placehold.it/1600x900" alt="">-->
-                    <img class="img-responsive" src="imagenes/red/quienesSomos/ventanaquienessomos2.jpg" alt="">
+                        <img class="img-responsive" src="imagenes/red/quienesSomos/ventanaquienessomos2.jpg" alt="">
                 </div>
             </div>   
         </div>
+
         @include('red.seccionpie')
 @stop
 
