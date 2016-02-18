08452328b0542701b92cba8d0eb0f22b93edddaf
Sonia Martinez <soniamartinezctr@gmail.com>
2016-02-18 17:50:01 -0600

    Cambios en back de áreas temáticas así como  asignación de css

--- resources/views/red/seccionareastematicas.blade.php
+++ resources/views/red/seccionareastematicas.blade.php
@@ -9,43 +9,43 @@
         </div>
     </div>
     <div class="col-sm-6 col-md-3 col-md-offset-1">
-		<div class="table-responsive">
-			<table>
-				<tr>
-					<td class="imgTabla"><a href="areastematicas#redesconocimiento">
-						<img id="imgRedesConocimiento" alt="imagen fija" src="imagenes/red/areasTematicas/RedesConocimiento.png" onmouseenter="imgResalta('RedesConocimiento')" onmouseleave="imgNormal('RedesConocimiento')"/></td>
-					</a>
-					<td class="txtTabla"><h3><strong>Redes de <br> conocimiento y <br> aprendizaje</strong></h3>
-					</td>
-				</tr>
-			</table>
-		</div>
+        <div class="table-responsive">
+            <table>
+                <tr>
+                    <td class="imgTabla"><a href="areastematicas#redesconocimiento">
+                        <img id="imgRedesConocimiento" alt="imagen fija" src="imagenes/red/areasTematicas/RedesConocimiento.png" onmouseenter="imgResalta('RedesConocimiento')" onmouseleave="imgNormal('RedesConocimiento')"/></td>
+                    </a>
+                    <td class="txtTabla"><h3><strong>Redes de <br> conocimiento y <br> aprendizaje</strong></h3>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
     <div class="col-sm-6 col-md-3">
-		<div class="table-responsive">
-			<table>
-				<tr>
-					<td class="imgTabla"><a href="areastematicas#modelos">
-						<img href="#" id="imgAmbientesEducativos" alt="imagen fija" src="imagenes/red/areasTematicas/ambientesEducativos.png" onmouseenter="imgResalta('AmbientesEducativos')" onmouseleave="imgNormal('AmbientesEducativos')"/></td>
-					</a>
-					<td class="txtTabla"><h3><strong>Modelos y <br> ambientes <br> educativos</strong></h3>
-					</td>
-				</tr>
-			</table>
-		</div>
+        <div class="table-responsive">
+            <table>
+                <tr>
+                    <td class="imgTabla"><a href="areastematicas#modelos">
+                        <img href="#" id="imgAmbientesEducativos" alt="imagen fija" src="imagenes/red/areasTematicas/ambientesEducativos.png" onmouseenter="imgResalta('AmbientesEducativos')" onmouseleave="imgNormal('AmbientesEducativos')"/></td>
+                    </a>
+                    <td class="txtTabla"><h3><strong>Modelos y <br> ambientes <br> educativos</strong></h3>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
     <div class="col-sm-6 col-md-3">
-		<div class="table-responsive">
-			<table>
-				<tr>
-					<td class="imgTabla"><a href="areastematicas#gestion">
-						<img id="imgGestioncalidad" alt="imagen fija" src="imagenes/red/areasTematicas/Gestioncalidad.png" onmouseenter="imgResalta('Gestioncalidad')" onmouseleave="imgNormal('Gestioncalidad')"/></td>
-					</a>
-					<td class="txtTabla"><h3><strong>Gestion y calidad <br> de programas <br> educativos</strong></h3>
-					</td>
-				</tr>
-			</table>
-		</div>
+        <div class="table-responsive">
+            <table>
+                <tr>
+                    <td class="imgTabla"><a href="areastematicas#gestion">
+                        <img id="imgGestioncalidad" alt="imagen fija" src="imagenes/red/areasTematicas/Gestioncalidad.png" onmouseenter="imgResalta('Gestioncalidad')" onmouseleave="imgNormal('Gestioncalidad')"/></td>
+                    </a>
+                    <td class="txtTabla"><h3><strong>Gestion y calidad <br> de programas <br> educativos</strong></h3>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 </div>
 <div class="row fondo_Claro">
@@ -54,30 +54,30 @@
 </div>
 <div class="row fondo_Claro">
     <div class="col-sm-6 col-md-4 col-md-offset-2">
-		<div class="table-responsive">
-			<table>
-				<tr>
-					<td class="imgTabla"><a href="areastematicas#multimedia">
-						<img href="#" id="imgMultimedia" alt="imagen fija" src="imagenes/red/areasTematicas/Multimedia.png" onmouseenter="imgResalta('Multimedia')" onmouseleave="imgNormal('Multimedia')"/></td>
-					</a>
-					<td class="txtTabla"><h3><strong>Multimedia y <br> desarrollo educativo</strong></h3>
-					</td>
-				</tr>
-			</table>
-		</div>
+        <div class="table-responsive">
+            <table>
+                <tr>
+                    <td class="imgTabla"><a href="areastematicas#multimedia">
+                        <img href="#" id="imgMultimedia" alt="imagen fija" src="imagenes/red/areasTematicas/Multimedia.png" onmouseenter="imgResalta('Multimedia')" onmouseleave="imgNormal('Multimedia')"/></td>
+                    </a>
+                    <td class="txtTabla"><h3><strong>Multimedia y <br> desarrollo educativo</strong></h3>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
     <div class="col-sm-6 col-md-4">
-		<div class="table-responsive">
-			<table>
-				<tr>
-					<td class="imgTabla"><a href="areastematicas#politica">
-						<img id="imgPoliticaEducativa" alt="imagen fija" src="imagenes/red/areasTematicas/PoliticaEducativa.png" onmouseenter="imgResalta('PoliticaEducativa')" onmouseleave="imgNormal('PoliticaEducativa')"/></td>
-					</a>
-					<td class="txtTabla"><h3><strong>Politica Educativa y <br> cambio social</strong></h3>
-					</td>
-				</tr>
-			</table>
-		</div>
+        <div class="table-responsive">
+            <table>
+                <tr>
+                    <td class="imgTabla"><a href="areastematicas#politica">
+                        <img id="imgPoliticaEducativa" alt="imagen fija" src="imagenes/red/areasTematicas/PoliticaEducativa.png" onmouseenter="imgResalta('PoliticaEducativa')" onmouseleave="imgNormal('PoliticaEducativa')"/></td>
+                    </a>
+                    <td class="txtTabla"><h3><strong>Politica Educativa y <br> cambio social</strong></h3>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 </div>
 <div class="row fondo_Claro">
