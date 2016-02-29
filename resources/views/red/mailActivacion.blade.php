493b994b50f724b76a32e0faf8859fadf59d51ff
Israel Toledo <j.israel.toledo@gmail.com>
2016-02-29 13:57:26 -0600

    Ya funciona el envío del correo de activación

--- resources/views/red/mailActivacion.blade.php
+++ resources/views/red/mailActivacion.blade.php
@@ -2,7 +2,8 @@
 <html>
     <body>
         Para activar tu cuenta da click en el siguiente enlace:<br>
-        <a href="http://redmite.televisioneducativa.gob.mx/activaCorreo/{{$correo}}/{{$hash}}"
+        <a href="http://redmite.televisioneducativa.gob.mx/activaCorreo/{{$correo}}/{{$hash}}"> 
+            <button type="button"> Activa tu cuenta</button></a>
         
     </body>
 </html>
