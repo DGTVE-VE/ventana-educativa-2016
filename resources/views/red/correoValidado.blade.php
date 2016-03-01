fb058722835c0a4b08eb381133cdc42a62201a45
Sonia Martinez <soniamartinezctr@gmail.com>
2016-03-01 12:24:27 -0600

    vistas correo

---
+++ resources/views/red/correoValidado.blade.php
@@ -0,0 +1,13 @@
+@section('title')
+Correo Validado
+@stop
+@extends('home')
+@section('menu')
+@include('red.seccionheader')
+@endsection
+@section('cuerpo')
+<div class="row">
+    <img src="imagenes/red/footer/activacionexitosa">
+</div>
+@include('red.seccionpie')
+@stop
--- resources/views/red/mailActivacion.blade.php
+++ resources/views/red/mailActivacion.blade.php
@@ -3,7 +3,7 @@
     <body>
         Para activar tu cuenta da click en el siguiente enlace:<br>
         <a href="http://redmite.televisioneducativa.gob.mx/activaCorreo/{{$correo}}/{{$hash}}"> 
-            <button type="button"> Activa tu cuenta</button></a>
-        
+            <img src="imagenes/red/footer/confirmacioncorreo.jpg">
+         </a>        
     </body>
 </html>
