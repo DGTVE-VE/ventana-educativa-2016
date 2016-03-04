738fa94f1c1de0368e1db6dc350e32206699a3ee
EDA1 <EDA1>
2016-03-04 17:22:16 -0600

    Página áreas temáticas. Cambio de formato para visualizar de forma distinta en resoluciones pequeñas.

--- resources/views/viewRed/paginaareastematicas.blade.php
+++ resources/views/viewRed/paginaareastematicas.blade.php
@@ -7,107 +7,131 @@ Publicaciones
 @endsection
 @section('cuerpoRedmite')
 <a name="redesconocimiento"></a>
-<div class="row backpareatema">
-    <div class=""> 
-        <div class="row" style="margin-top: 5%;" >  
-            <div class="row fondo_Obscuro">
-                <div class="col-md-12 text-center text-uppercase">
-                    <p class="pleca"> Áreas Temáticas</p>
-                </div>
-            </div>
-            <div class="col-md-2"></div>
-            <div class="col-md-8">
-                <table class="table table-responsive" style="margin-top: 5%;">
-                    <tr>
-                        <td></br><img src="imagenes/red/areasTematicas/redesdeconocimientoyaprendizaje-blanco.png"></td> 
-                        <td>
-                            <ul class="txtAreasTematicas">
-                                <li class="list-group-item-heading list-unstyled"><h3>Redes de Conocimiento y Aprendizaje</h3></li>
-                                <p style="text-align: justify;">Proponer modelos educativos acordes con los problemas y planteamientos teóricos, tales como: la teoría de las organizaciones para conformar comunidades educativas virtuales, representación y creación de conocimiento distribuido, aprendizaje colaborativo, redes de repositorios de conocimiento y de objetos de aprendizaje.</p>
-                                <p>Se identifican las siguientes líneas:</p>
-                                <a name="modelos"></a>
-                                <ul>
-                                    <li class=" list-unstyled"><span class="fa fa-circle-o"></span> Redes sociales para la educación. </li>
-                                    <li class=" list-unstyled"><span class="fa fa-circle-o"></span> El docente como generador del cambio: la educación digital y los retos en la región mesoamericana.</li>
-                                </ul>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></br><img src="imagenes/red/areasTematicas/modelosyambienteseducativos-blanco.png"></td>
-                        <td>
-                            <ul class="txtAreasTematicas">                            
-                                <li class="ist-group-item-heading list-unstyled"><h3>Modelos y Ambientes Educativos</h3></li>
-                                <p style="text-align: justify;">Esta área abarca las fases de investigación, fundamentación, desarrollo y evaluación de los modelos y ambientes educativos, basados en tecnologías de información y comunicación.</p>
-                                <p>Se identifican las siguientes líneas:</p>
-                                <a name="gestion"></a>  
-                                <ul>
-                                    <li class=" list-unstyled"><span class="fa fa-pencil"></span> Innovación, adaptación e integración de los sistemas educativos tecnológicos.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-pencil"></span> Diseño y desarrollo de sistemas educativos innovadores.</li>
-                                </ul>
-                            </ul>
-
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></br><img src="imagenes/red/areasTematicas/gestionycalidaddeprogramaseducativos-blanco.png"></td>
-                        <td>
-                            <ul  class="txtAreasTematicas">
-                                <li class="ist-group-item-heading list-unstyled"><h3>Gestión y Calidad de Programas Educativos</h3></li>
-                                <p style="text-align: justify;">Sus propósitos son el estudio y comparación de modelos de gestión institucional para el uso de las Tecnologías de la Información y Comunicación (TIC) en el sistema, así como para generar criterios e indicadores basados en la recuperación y documentación de buenas prácticas internacionales y regionales. De igual manera, identificar las problemáticas de los procesos de gestión y aseguramiento de la calidad.</p>
-                                <p>Se identifican las siguientes líneas:</p>
-                                <a name="multimedia"></a>  
-                                <ul>
-                                    <li class=" list-unstyled"><span class="fa fa-check"></span> Gestión de sistemas educativos con articulación de las modalidades y entornos físicos y virtuales.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-check"></span> Evaluación y calidad en sistemas y ambientes educativos.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-check"></span> Competencias docentes y directivas para la innovación educativa.</li>
-                                </ul>
-                            </ul>
-
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></br><img src="imagenes/red/areasTematicas/multimediaydesarrolloeducativo-blanco.png"></td>
-                        <td>
-                            <ul  class="txtAreasTematicas">
-                                <li class="ist-group-item-heading list-unstyled"><h3>Multimedia y Desarrollo Educativo</h3></li>
-                                <p style="text-align: justify;">Se explicarán y analizarán  las variables que intervienen en los procesos educativos, entre ellos: el desarrollo cognitivo, afectivo y social, que contribuyen con el desempeño de estudiantes, docentes y directivos. Asimismo, el impacto del desarrollo de software, objetos de aprendizaje, simuladores y laboratorios educativos; así como su incidencia en la calidad de los sistemas  educativos de la región mesoamericana.</p>
-                                <p>En esta línea se incluyen proyectos específicos relacionados con:</p>
-                                <a name="politica"></a>
-                                <ul>
-                                    <li class=" list-unstyled"><span class="fa fa-wifi"></span> Desarrollo, implementación y evaluación de materiales y recursos educativos digitales.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-wifi"></span> Construcción y administración de repositorios y plataformas educativas.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-wifi"></span> Educación mediada por tecnología y su contribución pedagógica.</li>
-                                </ul>
-                            </ul>  
-
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></br><img src="imagenes/red/areasTematicas/politicaeducativaycambiosocial-blanco.png"></td>
-                        <td>
-                            <ul  class="txtAreasTematicas">
-                                <li class="ist-group-item-heading list-unstyled"><h3>Política Educativa y Cambio Social</h3></li>
-                                <p style="text-align: justify;">Analizar la convergencia y articulación de las políticas públicas orientadas a la inserción de Tecnologías de la Información y Comunicación (TIC) en la educación básica, en los ámbitos internacional, nacional e institucional.</p>
-                                <p>En esta línea se incluyen proyectos específicos relacionados con:</p>
-                                <ul>
-                                    <li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Gobierno, política social y educativa.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Educación democrática.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Alfabetización e inclusión digital.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Ciudadanía digital.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Pedagogía digital.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Estudiantes y uso de las TIC.</li>
-                                    <li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Análisis de costo beneficios y oportunidades de acceso para la educación mediada por tecnología.</li>
-                                </ul>
-                            </ul> 
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div class="col-md-2"></div>
-        </div>
-        <br>
-    </div>
+<div class="row fondo_Obscuro" style="margin-top: 80px;">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center text-uppercase">
+		<p class="pleca"> Áreas Temáticas</p>
+	</div>
+</div>
+<div class="row backpareatema txtAreasTematicas" style="padding-top:50px;">
+	<div class="col-sm-1 col-md-1 col-lg-1"></div>
+	<div class="col-xs-4 col-sm-2 col-md-2 col-lg-2">
+		<img src="imagenes/red/areasTematicas/redesdeconocimientoyaprendizaje-blanco.png" style="position:absolute;">
+	</div>
+	<div class="col-xs-8 col-sm-7 col-md-7 col-lg-7">
+		<h3>Redes de Conocimiento y Aprendizaje</h3>
+		<br>
+	</div>
+	<div class="col-sm-12 col-md-12 col-lg-12">
+	</div>
+	<div class="col-sm-3 col-md-3 col-lg-3">
+	</div>
+	<div class="col-xs-12 col-sm-8 col-md-7 col-lg-7 text-justify">
+		<p>Proponer modelos educativos acordes con los problemas y planteamientos teóricos, tales como: la teoría de las organizaciones para conformar comunidades educativas virtuales, representación y creación de conocimiento distribuido, aprendizaje colaborativo, redes de repositorios de conocimiento y de objetos de aprendizaje.</p>
+		<p>Se identifican las siguientes líneas:</p>
+		<a name="modelos"></a>
+		<ul>
+			<li class=" list-unstyled"><span class="fa fa-circle-o"></span> Redes sociales para la educación. </li>
+			<li class=" list-unstyled"><span class="fa fa-circle-o"></span> El docente como generador del cambio: la educación digital y los retos en la región mesoamericana.</li>
+		</ul>
+	</div>
+</div>
+<div class="row backpareatema txtAreasTematicas" style="padding-top:50px;">
+	<div class="col-sm-1 col-md-1 col-lg-1"></div>
+	<div class="col-xs-4 col-sm-2 col-md-2 col-lg-2">
+		<img src="imagenes/red/areasTematicas/redesdeconocimientoyaprendizaje-blanco.png" style="position:absolute;">
+	</div>
+	<div class="col-xs-8 col-sm-7 col-md-7 col-lg-7">
+		<h3>Modelos y Ambientes Educativos</h3>
+		<br>
+	</div>
+	<div class="col-sm-12 col-md-12 col-lg-12">
+	</div>
+	<div class="col-sm-3 col-md-3 col-lg-3">
+	</div>
+	<div class="col-xs-12 col-sm-8 col-md-7 col-lg-7 text-justify">
+		<p>Esta área abarca las fases de investigación, fundamentación, desarrollo y evaluación de los modelos y ambientes educativos, basados en tecnologías de información y comunicación.</p>
+		<p>Se identifican las siguientes líneas:</p>
+		<a name="gestion"></a>  
+		<ul>
+			<li class=" list-unstyled"><span class="fa fa-pencil"></span> Innovación, adaptación e integración de los sistemas educativos tecnológicos.</li>
+			<li class=" list-unstyled"><span class="fa fa-pencil"></span> Diseño y desarrollo de sistemas educativos innovadores.</li>
+		</ul>
+	</div>
+</div>
+<div class="row backpareatema txtAreasTematicas" style="padding-top:50px;">
+	<div class="col-sm-1 col-md-1 col-lg-1"></div>
+	<div class="col-xs-4 col-sm-2 col-md-2 col-lg-2">
+		<img src="imagenes/red/areasTematicas/redesdeconocimientoyaprendizaje-blanco.png" style="position:absolute;">
+	</div>
+	<div class="col-xs-8 col-sm-7 col-md-7 col-lg-7">
+		<h3>Gestión y Calidad de Programas Educativos</h3>
+		<br>
+	</div>
+	<div class="col-sm-12 col-md-12 col-lg-12">
+	</div>
+	<div class="col-sm-3 col-md-3 col-lg-3">
+	</div>
+	<div class="col-xs-12 col-sm-8 col-md-7 col-lg-7 text-justify">
+		<p>Sus propósitos son el estudio y comparación de modelos de gestión institucional para el uso de las Tecnologías de la Información y Comunicación (TIC) en el sistema, así como para generar criterios e indicadores basados en la recuperación y documentación de buenas prácticas internacionales y regionales. De igual manera, identificar las problemáticas de los procesos de gestión y aseguramiento de la calidad.</p>
+		<p>Se identifican las siguientes líneas:</p>
+		<a name="multimedia"></a>  
+		<ul>
+			<li class=" list-unstyled"><span class="fa fa-check"></span> Gestión de sistemas educativos con articulación de las modalidades y entornos físicos y virtuales.</li>
+			<li class=" list-unstyled"><span class="fa fa-check"></span> Evaluación y calidad en sistemas y ambientes educativos.</li>
+			<li class=" list-unstyled"><span class="fa fa-check"></span> Competencias docentes y directivas para la innovación educativa.</li>
+		</ul>
+	</div>
+</div>
+<div class="row backpareatema txtAreasTematicas" style="padding-top:50px;">
+	<div class="col-sm-1 col-md-1 col-lg-1"></div>
+	<div class="col-xs-4 col-sm-2 col-md-2 col-lg-2">
+		<img src="imagenes/red/areasTematicas/redesdeconocimientoyaprendizaje-blanco.png" style="position:absolute;">
+	</div>
+	<div class="col-xs-8 col-sm-7 col-md-7 col-lg-7">
+		<h3>Multimedia y Desarrollo Educativo</h3>
+		<br>
+	</div>
+	<div class="col-sm-12 col-md-12 col-lg-12">
+	</div>
+	<div class="col-sm-3 col-md-3 col-lg-3">
+	</div>
+	<div class="col-xs-12 col-sm-8 col-md-7 col-lg-7 text-justify">
+		<p>Se explicarán y analizarán  las variables que intervienen en los procesos educativos, entre ellos: el desarrollo cognitivo, afectivo y social, que contribuyen con el desempeño de estudiantes, docentes y directivos. Asimismo, el impacto del desarrollo de software, objetos de aprendizaje, simuladores y laboratorios educativos; así como su incidencia en la calidad de los sistemas  educativos de la región mesoamericana.</p>
+		<p>En esta línea se incluyen proyectos específicos relacionados con:</p>
+		<a name="politica"></a>
+		<ul>
+			<li class=" list-unstyled"><span class="fa fa-wifi"></span> Desarrollo, implementación y evaluación de materiales y recursos educativos digitales.</li>
+			<li class=" list-unstyled"><span class="fa fa-wifi"></span> Construcción y administración de repositorios y plataformas educativas.</li>
+			<li class=" list-unstyled"><span class="fa fa-wifi"></span> Educación mediada por tecnología y su contribución pedagógica.</li>
+		</ul>
+	</div>
+</div>
+<div class="row backpareatema txtAreasTematicas" style="padding-top:50px; padding-bottom:50px;">
+	<div class="col-sm-1 col-md-1 col-lg-1"></div>
+	<div class="col-xs-4 col-sm-2 col-md-2 col-lg-2">
+		<img src="imagenes/red/areasTematicas/redesdeconocimientoyaprendizaje-blanco.png" style="position:absolute;">
+	</div>
+	<div class="col-xs-8 col-sm-7 col-md-7 col-lg-7">
+		<h3>Política Educativa y Cambio Social</h3>
+		<br>
+	</div>
+	<div class="col-sm-12 col-md-12 col-lg-12">
+	</div>
+	<div class="col-sm-3 col-md-3 col-lg-3">
+	</div>
+	<div class="col-xs-12 col-sm-8 col-md-7 col-lg-7 text-justify">
+		<p>Analizar la convergencia y articulación de las políticas públicas orientadas a la inserción de Tecnologías de la Información y Comunicación (TIC) en la educación básica, en los ámbitos internacional, nacional e institucional.</p>
+		<p>En esta línea se incluyen proyectos específicos relacionados con:</p>
+		<ul>
+			<li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Gobierno, política social y educativa.</li>
+			<li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Educación democrática.</li>
+			<li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Alfabetización e inclusión digital.</li>
+			<li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Ciudadanía digital.</li>
+			<li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Pedagogía digital.</li>
+			<li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Estudiantes y uso de las TIC.</li>
+			<li class=" list-unstyled"><span class="fa fa-puzzle-piece"></span> Análisis de costo beneficios y oportunidades de acceso para la educación mediada por tecnología.</li>
+		</ul>
+	</div>
 </div>
 @include('viewRed.seccionpie')
 @stop
--- resources/views/viewRed/paginaquienessomos.blade.php
+++ resources/views/viewRed/paginaquienessomos.blade.php
@@ -13,15 +13,15 @@ Quienes Somos
 			<p class="pleca">¿Quiénes Somos?</p>
 		</div>
 	</div>
-	<div class="row backpaquienes txtQuienesSomos" style="padding-top:2%; padding-bottom:2%;">
+	<div class="row backpaquienes txtQuienesSomos text-justify" style="padding-top:2%; padding-bottom:2%;">
 		<div class="col-xs-12 col-sm-5 col-sm-offset-1 col-md-5 col-md-offset-1 col-lg-5 col-lg-offset-1">
 			<h4>¿QUIÉNES SOMOS?</h4>
-			<p style="text-align: justify;">
+			<p>
 				Las Tecnologías de la Información y Comunicación (TIC) han modificado los modelos y las formas de 
 				comunicación entre las personas, el cómo hacen negocios, trabajan, estudian, se informan y se divierten,
 				por lo que se ha convertido en un elemento fundamental para la vida cotidiana.
 			</p>
-			<p style="text-align: justify;">
+			<p>
 				La Red Mesoamericana de Investigación y Tecnología Educativa, nace de las propuestas resultantes del
 				Primer Encuentro de Investigadores, Evaluadores y Técnicos en el Diseño de Recursos Educativos,
 				celebrado en Costa Rica, el pasado 2015; en donde se destacó la importancia de conformar un espacio
@@ -32,7 +32,7 @@ Quienes Somos
 				mesoamericana.
 			</p><br>
 			<h4>MISIÓN</h4>
-			<p style="text-align: justify;">
+			<p>
 				Integrar una red de expertos acádemicos e investigadores que analicen, propongan y compartan
 				aspectos revelantes para la generación de una educación básica de calidad e inclusiva en la región
 				mesoamericana, a través de investigación, expeiencias, modelos pedagógicos y nuevas metodologías 
@@ -48,7 +48,7 @@ Quienes Somos
 		</div>
 		<div class="col-xs-12 col-sm-5 col-md-5 col-lg-5" style="padding-left:5%">
 			<h4>OBJETIVOS GENERALES</h4>
-			<P style="text-align: justify;">
+			<P>
 				Integrar un espacio para el encuentro, difusión e impulso de la investigación educativa por
 				acádemicos y profesionales, enfocada al uso y aprovechamiento de las Tecnologías de la
 				Información y Comunicación (TIC) en la educación básica, así como para la identificación de nuevos
