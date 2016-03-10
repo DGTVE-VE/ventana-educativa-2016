1b4f33dba13989d850942406b774240768a25da1
Sonia Martinez <soniamartinezctr@gmail.com>
2016-03-10 15:15:24 -0600

    cambios presentación y encabezado

--- resources/views/viewVentana/encabezadoVentana.blade.php
+++ resources/views/viewVentana/encabezadoVentana.blade.php
@@ -10,39 +10,58 @@
     <div class="navbar-collapse collapse">
         <ul class="nav navbar-nav navbar-right">
             <li class="col-md-pull-12 col-xs-pull-4 col-sm-pull-2 libuscar">
-                <form id="divbuscar" class="navbar-search">
-                    <input type="search" class="buscar" placeholder="Search">
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="input-group">
+                        <input type="text" class="form-control inputSearch" placeholder="Buscar...">
+                        <span class="input-group-btn ">
+                            <button type="submit" class="btn btn-default">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                    </div>
                 </form>
             </li>
-            <li class="dropdown">
-                 <a href="#" class="dropdown-toggle iconoAppsImg" data-toggle="dropdown">
-                <img class="iconoApps" src="imagenes/ventana/encabezado/iconoApps.png">
-            </a>
-            <ul class="dropdown-menu image-responsive menuVentanaApps">
-                <li class="text-center">
-                    <a class="" href="#">
-                        <img src="imagenes/ventana/encabezado/logoventana.png" height="50" width="150">
-                    </a>
-                </li>
-                <hr>
-                <li class="text-center">
-                    <a class="" href="redmite">
-                        <img src="imagenes/red/LogoRed/logoredmesoamericana.png" height="50" width="150">
-                    </a>
-                </li>
-                <hr>
-                <li class="text-center">
-                    <a class="" href="#">
-                        <img src="imagenes/ventana/encabezado/mx.png" height="50" width="150">
-                    </a>
-                </li>
-                <hr>
-            </ul>
+            <li class="dropdown liApp">
+                <div class=" divli dropdown-toggle" data-toggle="dropdown">
+                    <img class="iconoApps"  src="imagenes/ventana/encabezado/iconoApps.png">
+                </div>
+                <ul class="dropdown-menu image-responsive menuVentanaApps">
+                    <table>
+                        <tr>
+                            <td class="divApp">
+                                <a class="" href="#"><img src="http://placehold.it/50x50"></a>
+                                <label class="etiquetaApp">Ventana</label>
+                            </td>
+                            <td class="divApp">
+                                <a class="" href="#"><img src="http://placehold.it/50x50"></a>
+                                <label class="etiquetaApp">Ventana</label>
+                            </td>
+                            <td class="divApp">
+                                <a class="" href="#"><img src="http://placehold.it/50x50"></a>
+                                <label class="etiquetaApp">Ventana</label>
+                            </td>                            
+                        </tr>
+                        <tr>
+                            <td class="divApp">
+                                <a class="" href="#"><img src="http://placehold.it/50x50"></a>
+                                <label class="etiquetaApp">Ventana</label>
+                            </td>
+                            <td class="divApp">
+                                <a class="" href="#"><img src="http://placehold.it/50x50"></a>
+                                <label class="etiquetaApp">Ventana</label>
+                            </td>
+                            <td class="divApp">
+                                <a class="" href="#"><img src="http://placehold.it/50x50"></a>
+                                <label class="etiquetaApp">Ventana</label>
+                            </td>                            
+                        </tr>                        
+                    </table>                                       
+                </ul>
             </li>
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <div class=" divli dropdown-toggle" data-toggle="dropdown">
                     <div class="fotoUsuario text-center">R</div>
-                </a>
+                </div>
                 <ul class="dropdown-menu fondoRegistro">
                     <li class="panel-body">
                         <div class="row">
--- resources/views/viewVentana/presentacionVentana.blade.php
+++ resources/views/viewVentana/presentacionVentana.blade.php
@@ -7,12 +7,11 @@ Ventana Educativa
 @endsection
 @section('cuerpoVentana')
 <div class="container">
-        <div id="presentacion" class="carousel slide">
-          <div class="carousel-inner">           
+    <div id="presentacion" class="carousel slide">
+        <div class="carousel-inner">           
             <div class="item active">
                 <img class="image-responsive" src="imagenes/ventana/presentacion/lamina1.png" alt="Slide1">              
             </div>
-            
             <div class="item">
                 <img class="image-responsive" src="imagenes/ventana/presentacion/lamina2.png" alt="Slide2">
             </div>
@@ -21,19 +20,37 @@ Ventana Educativa
             </div>
             <div class="item">
                 <img class="image-responsive" src="imagenes/ventana/presentacion/lamina4.png" alt="Slide2">
+            </div>       
+            <div class="item">
+                <img class="image-responsive" src="imagenes/ventana/presentacion/lamina5.png" alt="Slide2">
+            </div>  
+            <div class="item">
+                <img class="image-responsive" src="imagenes/ventana/presentacion/lamina6.png" alt="Slide2">
+            </div>  
+            <div class="item">
+                <img class="image-responsive" src="imagenes/ventana/presentacion/lamina7.png" alt="Slide2">
+            </div>  
+            <div class="item">
+                <img class="image-responsive" src="imagenes/ventana/presentacion/lamina8.png" alt="Slide2">
+            </div>    
+            <div class="item">
+                <img class="image-responsive" src="imagenes/ventana/presentacion/lamina8.png" alt="Slide2">
+            </div>  
+            <div class="item">
+                <img class="image-responsive" src="imagenes/ventana/presentacion/lamina8.png" alt="Slide2">
             </div>              
-          </div>          
-              <div class="carousel-controls">
-              	<a class="left carousel-control" href="#presentacion" data-slide="prev"><i class="fa fa-angle-double-left fa-lg"></i></a>
-              	<a class="right carousel-control" href="#presentacion" data-slide="next"><i class="fa fa-angle-double-right fa-lg"></i></a>
-              </div>
-          
-        </div>   
+        </div>          
+        <div class="carousel-controls">
+            <a class="left carousel-control" href="#presentacion" data-slide="prev"><i class="fa fa-angle-double-left fa-lg"></i></a>
+            <a class="right carousel-control" href="#presentacion" data-slide="next"><i class="fa fa-angle-double-right fa-lg"></i></a>
+        </div>
+
+    </div>   
 </div>
 <script>
-	$('#presentacion').carousel({
-		interval:   false;
-	});
+    $('#presentacion').carousel({
+        interval: false;
+    });
 </script>
 @endsection
 @section('pieVentana')
