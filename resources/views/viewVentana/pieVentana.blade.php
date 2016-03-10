92f16ba33e97dd0ceaac070a48363040b0b8e3ad
grisvelazquez <grisvelazquez.v@gmail.com>
2016-03-10 11:09:38 -0600

    cambiar scrip a indexVentana

--- resources/views/viewVentana/pieVentana.blade.php
+++ resources/views/viewVentana/pieVentana.blade.php
@@ -1,10 +1,7 @@
- <div class="tabs_holder f1 "style="margin-top: 3%;">
+ <div class="tabs_holder f1"style="margin-top: 3%;">
             <ul >
                 <li ><a href="#your-tab-id-1">COLABORADORES</a></li>
-
             </ul>
-     <br/>
-     
             <div class="row">
                 <div class="col-md-2">
                     <a href="#" data-toggle="tab" class=""> 
@@ -38,10 +35,8 @@
                 </div>
             </div>
         </div>
-
-        <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.min.js"></script>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="http://onlinehtmltools.com/tab-generator/skinable_tabs.min.js"></script>
+      
+        
         <script type="text/javascript">
             $('.tabs_holder').skinableTabs({
                 effect: 'overlapping',
