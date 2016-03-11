3b71068d4deb826b2dbadc4ffee6f0dd456522b1
grisvelazquez <grisvelazquez.v@gmail.com>
2016-03-11 16:47:36 -0600

    cambio de tamaño de imagenes de colaboradores

--- resources/views/viewVentana/pieVentana.blade.php
+++ resources/views/viewVentana/pieVentana.blade.php
@@ -39,10 +39,10 @@
 	</div>
 </div>
         
-<script type="text/javascript">
+<!--<script type="text/javascript">
 	$('.tabs_holder').skinableTabs({
 		effect: 'overlapping',
 		skin: 'skin3',
 		position: 'bottom'
 	});
-</script>
+</script>-->
