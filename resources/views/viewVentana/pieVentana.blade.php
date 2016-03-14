1ada1a4aa1abe31f0f4ba1e10d6b1c9ef933f486
Sonia Martinez <soniamartinezctr@gmail.com>
2016-03-14 17:32:30 -0600

    cambio en pie de pagina por tabla en Home

--- resources/views/viewVentana/pieVentana.blade.php
+++ resources/views/viewVentana/pieVentana.blade.php
@@ -5,6 +5,65 @@
             <li ><div class="fondoColabora "><a href="#"><p class="txtPestana ">COLABORADORES</p></a></div></li>
         </ul>
         <div class="row " id="efectobarramenu">
+            <table class="table" style=" border-top-style: none !important;">
+                <tr>
+                    <td>
+                        <a href="" onMouseOver="document.MyImage0.src = 'imagenes/ventana/pieventana/sepcolor.png';"
+                           onMouseOut="document.MyImage0.src = 'imagenes/ventana/pieventana/sep.png';">
+                            <img class="img-responsive" src="imagenes/ventana/pieventana/sep.png" style="margin-left: 20% !important;" name="MyImage0">
+                        </a> 
+                    </td>
+                    <td>
+                        <a href="" onMouseOver="document.MyImage1.src = 'imagenes/ventana/pieventana/tvecolor.png';"
+                           onMouseOut="document.MyImage1.src = 'imagenes/ventana/pieventana/tve.png';">
+                            <img class="img-responsive" src="imagenes/ventana/pieventana/tve.png" style="margin-left: -10% !important;" name="MyImage1" >
+                        </a> 
+                    </td>
+                    <td>
+                        <a href="#" onMouseOver="document.MyImage2.src = 'imagenes/ventana/pieventana/csucacolor.png';"
+                           onMouseOut="document.MyImage2.src = 'imagenes/ventana/pieventana/csuca.png';">
+                            <img class="img-responsive" src="imagenes/ventana/pieventana/csuca.png" style="margin-left: -35% !important;" name="MyImage2">
+                        </a>       
+                    </td>
+                    <td>
+                        <a href="" onMouseOver="document.MyImage3.src = 'imagenes/ventana/pieventana/ceccsicacolor.png';"
+                           onMouseOut="document.MyImage3.src = 'imagenes/ventana/pieventana/ceccsica.png';">
+                            <img class="img-responsive" src="imagenes/ventana/pieventana/ceccsica.png" style="margin-left: 30% !important;" name="MyImage3">
+                        </a>    
+                    </td>
+                    <td>
+                        <a href="" onMouseOver="document.MyImage4.src = 'imagenes/ventana/pieventana/unahcolor.png';"
+                           onMouseOut="document.MyImage4.src = 'imagenes/ventana/pieventana/unah.png';">
+                            <img class="img-responsive" src="imagenes/ventana/pieventana/unah.png" style="margin-left: 30% !important;" name="MyImage4">
+                        </a> 
+                    </td>
+                    <td>
+                        <a href="" onMouseOver="document.MyImage5.src = 'imagenes/ventana/pieventana/mepcolor.png';"
+                           onMouseOut="document.MyImage5.src = 'imagenes/ventana/pieventana/mep.png';">
+                            <img class="img-responsive" src="imagenes/ventana/pieventana/mep.png" style="margin-left: 30% !important;"  name="MyImage5">
+                        </a> 
+                    </td>
+                    <td>
+                        <a href="" onMouseOver="document.MyImage6.src = 'imagenes/ventana/pieventana/amexcidcolor.png';"
+                           onMouseOut="document.MyImage6.src = 'imagenes/ventana/pieventana/amexcid.png';">
+                            <img class="img-responsive" src="imagenes/ventana/pieventana/amexcid.png" name="MyImage6" width="120" height="100" style="margin-top: 5%; margin-left: 20% !important;">
+                        </a> 
+                    </td>
+                </tr>
+            </table>
+    </div>
+</div>
+</div>
+
+
+<!--
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 tabs_holder f1 navbar-fixed-bottom">
+
+        <ul>
+            <li ><div class="fondoColabora "><a href="#"><p class="txtPestana ">COLABORADORES</p></a></div></li>
+        </ul>
+        <div class="row " id="efectobarramenu">
 
             <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1" >
                 <a href="" onMouseOver="document.MyImage0.src = 'imagenes/ventana/pieventana/sepcolor.png';"
@@ -57,6 +116,6 @@
             </div>
         </div>
     </div>
-</div>
+</div>-->
 <script>
     $('#efectobarramenu').addClass('animated fadeInDown');</script>
