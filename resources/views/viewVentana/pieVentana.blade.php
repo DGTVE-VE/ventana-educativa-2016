d8bee9d9bdb9112d166a7e525e380b95ab539adc
grisvelazquez <grisvelazquez.v@gmail.com>
2016-03-14 16:33:11 -0600

    dar formato al código

--- resources/views/viewVentana/pieVentana.blade.php
+++ resources/views/viewVentana/pieVentana.blade.php
@@ -1,29 +1,19 @@
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 tabs_holder f1 navbar-fixed-bottom">
-       
+
         <ul>
             <li ><div class="fondoColabora "><a href="#"><p class="txtPestana ">COLABORADORES</p></a></div></li>
         </ul>
-       
-        
         <div class="row ">
-            <div>
-                <div class="col-xs-3 col-sm-2 col-md-2 col-lg-2" >
-                <!--		<a href="#" data-toggle="tab"> 
-                                        <div id="imagen-tve" class="img-responsive"></div>
-                                </a>  
-                        </div>                       -->
+
+            <div class="col-xs-3 col-sm-2 col-md-2 col-lg-2" >
                 <a href="" onMouseOver="document.MyImage0.src = 'imagenes/ventana/pieventana/sepcolor.png';"
                    onMouseOut="document.MyImage0.src = 'imagenes/ventana/pieventana/sep.png';">
                     <img class="img-responsive" src="imagenes/ventana/pieventana/sep.png" name="MyImage0">
                 </a> 
             </div>           
-                    
+
             <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1" >
-                <!--		<a href="#" data-toggle="tab"> 
-                                        <div id="imagen-tve" class="img-responsive"></div>
-                                </a>  
-                        </div>                       -->
                 <a href="" onMouseOver="document.MyImage1.src = 'imagenes/ventana/pieventana/tvecolor.png';"
                    onMouseOut="document.MyImage1.src = 'imagenes/ventana/pieventana/tve.png';">
                     <img class="img-responsive" src="imagenes/ventana/pieventana/tve.png" name="MyImage1" >
@@ -31,66 +21,40 @@
             </div>           
             <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" ></div>
             <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-
                 <a href="#" onMouseOver="document.MyImage2.src = 'imagenes/ventana/pieventana/csucacolor.png';"
                    onMouseOut="document.MyImage2.src = 'imagenes/ventana/pieventana/csuca.png';">
                     <img class="img-responsive" src="imagenes/ventana/pieventana/csuca.png" name="MyImage2">
-
                 </a>
-
-
             </div>
             <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" ></div>
             <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                <!--                <a href="#" data-toggle="tab">
-                                    <div id="imagen-ceccsica" class="img-responsive"></div>
-                                </a>-->
-
                 <a href="" onMouseOver="document.MyImage3.src = 'imagenes/ventana/pieventana/ceccsicacolor.png';"
                    onMouseOut="document.MyImage3.src = 'imagenes/ventana/pieventana/ceccsica.png';">
                     <img class="img-responsive" src="imagenes/ventana/pieventana/ceccsica.png" name="MyImage3">
                 </a> 
-
-
             </div>
             <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" ></div>
             <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                <!--                <a href="#" data-toggle="tab">
-                                    <div id="imagen-unah" class="img-responsive"></div>
-                                </a>-->
-
                 <a href="" onMouseOver="document.MyImage4.src = 'imagenes/ventana/pieventana/unahcolor.png';"
                    onMouseOut="document.MyImage4.src = 'imagenes/ventana/pieventana/unah.png';">
                     <img class="img-responsive" src="imagenes/ventana/pieventana/unah.png" name="MyImage4">
                 </a> 
 
             </div>
-             <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" ></div>
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" ></div>
             <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                <!--                <a href="#" data-toggle="tab">
-                                    <div id="imagen-mep" class="img-responsive"></div>
-                                </a>-->
-
                 <a href="" onMouseOver="document.MyImage5.src = 'imagenes/ventana/pieventana/mepcolor.png';"
                    onMouseOut="document.MyImage5.src = 'imagenes/ventana/pieventana/mep.png';">
                     <img class="img-responsive" src="imagenes/ventana/pieventana/mep.png" height="55%" width="55%" name="MyImage5">
                 </a> 
- 
-            </div>
+               </div>
             <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" ></div>
             <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                <!--                <a href="#" data-toggle="tab">
-                                    <div id="imagen-amexcid" class="img-responsive"></div>
-                                </a>-->
-
-                <a href="" onMouseOver="document.MyImage6.src = 'imagenes/ventana/pieventana/amexcidcolor.png';"
+               <a href="" onMouseOver="document.MyImage6.src = 'imagenes/ventana/pieventana/amexcidcolor.png';"
                    onMouseOut="document.MyImage6.src = 'imagenes/ventana/pieventana/amexcid.png';">
                     <img class="img-responsive" src="imagenes/ventana/pieventana/amexcid.png" name="MyImage6">
                 </a> 
-
-                
             </div>
-      </div>
         </div>
     </div>
 </div>
