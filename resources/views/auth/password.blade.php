f68c41f7c29378c1f595be00d987568649c919b0
Sonia Martinez <soniamartinezctr@gmail.com>
2016-03-16 10:30:11 -0600

    Cambios en encabezado registro y presentacion de ventana home

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,15 +1,33 @@
+@extends('indexVentana')
+@section('titleVentana')
+Ventana Educativa
+@stop
+
+@section('menuVentana')
+@include('viewVentana.encabezadoVentana')
+@endsection
+@section('cuerpoVentana')
 @if (session('status'))
 Te llegará un email con la liga para que cambies tu password.
 @else
-<form method="POST" action="{{url ('password/email')}}">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" class="form-control" name="email" value="">        
-    </div>
-    <button type="submit" class="btn btn-primary">
-        Envíame un email para resetear mi password.
-    </button>
-</form>
-
+<div class = "panel panel-default frmRegistro col-lg-offset-2 col-md-8">
+    <br><br>
+    <br><br>
+    <form method="POST" action="{{url ('password/email')}}">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" name="email" value="">        
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Envíame un email para resetear mi password.
+        </button>
+    </form>
+    <br><br><br><br>
+    <br><br>
+</div>
 @endif
+@endsection
+@section('pieVentana')
+@include('viewVentana.pieVentana')
+@endsection
--- resources/views/viewVentana/pieVentana.blade.php
+++ resources/views/viewVentana/pieVentana.blade.php
@@ -10,7 +10,8 @@
                     <td>
                         <a href=" http://www.gob.mx/sep" target="_blank" onMouseOver="document.MyImage0.src = 'imagenes/ventana/pieventana/sepcolor.png';"
                            onMouseOut="document.MyImage0.src = 'imagenes/ventana/pieventana/sep.png';">
-                            <img class="img-responsive" src="imagenes/ventana/pieventana/sep.png" style="margin-left: 20% !important;" name="MyImage0">
+                            {{ HTML::image('imagenes/ventana/pieventana/sep.png','Logo sep', ['class'=>'logo-sep', 'style'=>'margin-left: 20% !important;']) }}
+                            <!--<img class="img-responsive" src="{!! HTML::image('imagenes/ventana/pieventana/sep.png') !!}" style=";" name="MyImage0">-->
                         </a> 
                     </td>
                     <td>
@@ -22,7 +23,8 @@
                     <td>
                         <a href=" http://www.csuca.org/" target="_blank" onMouseOver="document.MyImage2.src = 'imagenes/ventana/pieventana/csucacolor.png';"
                            onMouseOut="document.MyImage2.src = 'imagenes/ventana/pieventana/csuca.png';">
-                            <img class="img-responsive" src="imagenes/ventana/pieventana/csuca.png" style="margin-left: -35% !important;" name="MyImage2">
+                            {{ HTML::image("imagenes/ventana/pieventana/csuca.png", "Logo") }}
+                            <!--<img class="img-responsive" src="{!! HTML::image('imagenes/ventana/pieventana/csuca.png') !!}" style="margin-left: -35% !important;" name="MyImage2">-->
                         </a>       
                     </td>
                     <td>
