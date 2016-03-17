b8635ce8ca6f2268ba86812dfc05851098e88c6f
Sonia Martinez <soniamartinezctr@gmail.com>
2016-03-17 09:55:06 -0600

    cambio en rutas a código laravel

--- resources/views/indexVentana.blade.php
+++ resources/views/indexVentana.blade.php
@@ -13,7 +13,8 @@ and open the template in the editor.
         <meta name="author" content="Ecosistema Digital de Aprendizaje (DGTVE)">
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
-        <link rel="stylesheet" href="css/red/estiloredmesoamericana.css">
+        <!--<link rel="stylesheet" href="css/red/estiloredmesoamericana.css">-->
+        <link rel="stylesheet" href="{{ asset('css/red/estiloredmesoamericana.css') }}" >
         <link rel="shortcut icon" href="{{ asset('redmite1.ico') }}" >
 
         <!-- Latest compiled and minified CSS -->
@@ -24,51 +25,62 @@ and open the template in the editor.
         <!--	-------------------------- Fuentes de google fonts ------------------------------------		-->
         <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,300,500,700' rel='stylesheet' type='text/css'>
 
-        <link rel="stylesheet" href="css/red/ihover.css">  
-        <link rel="stylesheet" href="css/red/animate.css">
-    <!--<link type="text/css" rel="stylesheet" href="http://onlinehtmltools.com/tab-generator/skins/skin3/bottom.css">-->
+<!--        <link rel="stylesheet" href="css/red/ihover.css">  
+        <link rel="stylesheet" href="css/red/animate.css">-->
+        <link rel="stylesheet" href="{{ asset('css/red/ihover.css') }}" >
+        <link rel="stylesheet" href="{{ asset('css/red/animate.css') }}" >        
 
-        <link rel="stylesheet" href="css/ventana/estiloventana2.css">  
-        <link rel="stylesheet" href="css/ventana/estiloventana3.css"> 
+        <!--<link type="text/css" rel="stylesheet" href="http://onlinehtmltools.com/tab-generator/skins/skin3/bottom.css">-->
 
-        
-        
+        <!--        <link rel="stylesheet" href="css/ventana/estiloventana2.css">  
+                <link rel="stylesheet" href="css/ventana/estiloventana3.css"> -->
+
+        <link rel="stylesheet" href="{{ asset('css/ventana/estiloventana2.css') }}" >
+        <link rel="stylesheet" href="{{ asset('css/ventana/estiloventana3.css') }}" >
+
         <!--CDN FormHelpers-->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-formhelpers/2.3.0/css/bootstrap-formhelpers.css" rel="stylesheet"/>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-formhelpers/2.3.0/css/bootstrap-formhelpers.min.css" rel="stylesheet"/>
         <!--fin FormHelpers -->
-        
-       
-        
+
+
+
         <!-- jQuery library -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-        
+
         <!-- Latest compiled JavaScript -->
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-formhelpers/2.3.0/js/bootstrap-formhelpers.js" type="text/javascript"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-formhelpers/2.3.0/js/bootstrap-formhelpers.min.js" type="text/javascript"></script>
-        
+
         <!--script src="js/ventana/bootstrap-datepicker.js" type="text/javascript"></script-->
-        
+
         <!--script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/jquery-ui.min.js"></script--> <!-- efecto skin3 mení pie-->
         <!--<script type="text/javascript" src="http://onlinehtmltools.com/tab-generator/skinable_tabs.min.js"></script>  efecto skin3 mení pie-->
     </head>
     <body style="background-image:url('{{ asset('imagenes/ventana/background/backgroundDefault.jpg') }}');">     
         <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-        ga('create', 'UA-75232922-1', 'auto');
-        ga('send', 'pageview');
+ga('create', 'UA-75232922-1', 'auto');
+ga('send', 'pageview');
 
-      </script>
+        </script>
         <header>
             @yield('menuVentana')
         </header>
-      <div class="container-fluid">
+        <div class="container-fluid">
             @yield('cuerpoVentana')
         </div>
         <div>
