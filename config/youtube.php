843884dc56306ef39534967305688eff11ade9b4
Sonia Martinez <soniamartinezctr@gmail.com>
2016-03-30 15:54:46 -0600

    Se cambió la API Key

--- config/youtube.php
+++ config/youtube.php
@@ -4,6 +4,6 @@
 // You can find the keys here : https://console.developers.google.com
 
 return array(
-    'KEY' => 'AIzaSyD6qw7LMNBZi7OP6l45ALxl3ad-BRq5bRU'
+    'KEY' => 'AIzaSyBc5-sIZ0xVCNqOD5orQ2pIKlXaFgT9-Ms'
               
 );
