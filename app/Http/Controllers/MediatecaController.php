ecfa8bed0fa0ca58d9d5049a812570a88ce8613c
Sonia Martinez <soniamartinezctr@gmail.com>
2016-03-30 18:02:16 -0600

    Agregar test de api de youtube

--- app/Http/Controllers/MediatecaController.php
+++ app/Http/Controllers/MediatecaController.php
@@ -3,53 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
-class MediatecaController extends Controller
-{
+class MediatecaController extends Controller {
+
     public function mediateca() {
-       return view('viewMediateca/mediateca');
+        return view('viewMediateca/mediateca');
     }
+
     public function telesecundaria() {
-       return view('viewMediateca/telesecundaria');
+        return view('viewMediateca/telesecundaria');
     }
+
     public function telebachillerato() {
-       return view('viewMediateca/telebachillerato');
+        return view('viewMediateca/telebachillerato');
     }
+
     public function primergrado() {
-       return view('viewMediateca/primerGrado');
+        return view('viewMediateca/primerGrado');
     }
+
     public function segundogrado() {
-       return view('viewMediateca/segundoGrado');
+        return view('viewMediateca/segundoGrado');
     }
-	public function tercergrado() {
-       return view('viewMediateca/tercerGrado');
+
+    public function tercergrado() {
+        return view('viewMediateca/tercerGrado');
     }
-	public function semestreI() {
-       return view('viewMediateca/semestreI');
+
+    public function semestreI() {
+        return view('viewMediateca/semestreI');
     }
-	public function semestreII() {
-       return view('viewMediateca/semestreII');
+
+    public function semestreII() {
+        return view('viewMediateca/semestreII');
     }
-	public function semestreIII() {
-       return view('viewMediateca/semestreIII');
+
+    public function semestreIII() {
+        return view('viewMediateca/semestreIII');
     }
-	public function semestreIV() {
-       return view('viewMediateca/semestreIV');
+
+    public function semestreIV() {
+        return view('viewMediateca/semestreIV');
     }
-	public function semestreV() {
-       return view('viewMediateca/semestreV');
+
+    public function semestreV() {
+        return view('viewMediateca/semestreV');
     }
-	public function semestreVI() {
-       return view('viewMediateca/semestreVI');
+
+    public function semestreVI() {
+        return view('viewMediateca/semestreVI');
     }
-    	public function videos() {
-       return view('viewMediateca/videos');
+
+    public function videos() {
+        return view('viewMediateca/videos');
     }
-    
-    public function test (){
+
+    public function test() {
         $video = \Alaouy\Youtube\Facades\Youtube::getVideoInfo('rie-hPVJ7Sw');
         var_dump ($video);
     }
+
 }
