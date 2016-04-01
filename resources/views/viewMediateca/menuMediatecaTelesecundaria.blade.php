78ebbb728bfc6fc617a20c376a034b9239bba4ce
grisvelazquez <grisvelazquez.v@gmail.com>
2016-04-01 16:54:16 -0600

    agregar ventana modal para el calendario escolar de telesecundaria

--- resources/views/viewMediateca/menuMediatecaTelesecundaria.blade.php
+++ resources/views/viewMediateca/menuMediatecaTelesecundaria.blade.php
@@ -1,4 +1,4 @@
-<div class="row" style="left: 0%;">
+<div class="row">
     <div id="wrapper">
         <div class="overlay"></div>
 
@@ -7,12 +7,24 @@
             <ul class="nav sidebar-nav">
                 <li><a href="http://telesecundaria.dgmie.sep.gob.mx/mision/" target="_blank">Misión</a></li>
                 <li><a href="http://telesecundaria.dgmie.sep.gob.mx/mapa/" target="_blank">Mapa Curricular</a></li>
-                <li><a href="#">Calendario</a></li>
+                <li><a data-toggle="modal" href="#myModal">Calendario</a></li>
                 <li><a href="http://telesecundaria.dgmie.sep.gob.mx/materiales/" tarjet="_blank">Materiales Educativos</a></li>
                 <li id="sinBorde"><a href="http://telesecundaria.dgmie.sep.gob.mx/programacion/index.php" target="_blank">Programación Televisiva</a></li>
             </ul>
         </nav>
+         <!-- Ventana modal para calendario escolar -->
+                <div class="modal fade" id="myModal" role="dialog">
+                  <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                          <div class="modal-body">
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <iframe class=""src="http://www.enba.sep.gob.mx/files/calendario2015-2016.pdf" width="870" height="560" frameborder="0"></iframe> 
+                      </div>
+                    </div>
+                  </div>
+                </div> <!-- Fin modal -->
         <!-- /#sidebar-wrapper -->
+       
         <!-- Page Content -->
         <div id="page-content-wrapper">
             <button type="button" class="hamburger is-closed" data-toggle="offcanvas" >
@@ -21,7 +33,7 @@
         </div> <!-- /#page-content-wrapper -->
     </div> <!-- /#wrapper -->
 </div>
-
+ 
 <script>
 
     $(document).ready(function () {
@@ -54,6 +66,6 @@
     });
 
 </script>
-
+ 
 
 
