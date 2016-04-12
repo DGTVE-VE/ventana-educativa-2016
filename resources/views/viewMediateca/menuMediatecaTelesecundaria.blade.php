b690257494db60cf0636498bf98caf477840aa1a
grisvelazquez <grisvelazquez.v@gmail.com>
2016-04-12 16:54:47 -0500

    cambiar como imagen el calendario de educamedia

--- resources/views/viewMediateca/menuMediatecaTelesecundaria.blade.php
+++ resources/views/viewMediateca/menuMediatecaTelesecundaria.blade.php
@@ -5,11 +5,11 @@
         <div class="margensuperiormenuTS">
             <nav class="navbar navbar-inverse navbar-fixed-top margensuperiormenuTS" id="sidebar-wrapper" role="navigation">
                 <ul class="nav sidebar-nav">
-                    <li><a href="http://telesecundaria.dgmie.sep.gob.mx/mision/" target="_blank">Misión</a></li>
-                    <li><a href="http://telesecundaria.dgmie.sep.gob.mx/mapa/" target="_blank">Mapa Curricular</a></li>
+                    <li><a href="{{url('http://telesecundaria.dgmie.sep.gob.mx/mision/')}}" target="_blank">Misión</a></li>
+                    <li><a href="{{url('http://telesecundaria.dgmie.sep.gob.mx/mapa/')}}" target="_blank">Mapa Curricular</a></li>
                     <li><a data-toggle="modal" href="#myModal-telesec">Calendario</a></li>
-                    <li><a href="http://telesecundaria.dgmie.sep.gob.mx/materiales/" target="_blank">Materiales Educativos</a></li>
-                    <li id="sinBorde"><a href="http://www.televisioneducativa.gob.mx/canales/telesecundaria" target="_blank">Programación Televisiva</a></li>
+                    <li><a href="{{url('http://telesecundaria.dgmie.sep.gob.mx/materiales/')}}" target="_blank">Materiales Educativos</a></li>
+                    <li id="sinBorde"><a href="{{url('http://www.televisioneducativa.gob.mx/canales/telesecundaria')}}" target="_blank">Programación Televisiva</a></li>
                 </ul>
             </nav>
             <!-- /#sidebar-wrapper -->
@@ -21,18 +21,21 @@
                 </button>
             </div> <!-- /#page-content-wrapper -->
 
-
             <!-- Ventana modal para calendario escolar -->
             <div class="modal fade" id="myModal-telesec" role="dialog">
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
-                        <div class="modal-body">
+                        <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <iframe class=""src="http://www.enba.sep.gob.mx/files/calendario2015-2016.pdf" width="870" height="560" ></iframe> 
+                        </div>
+                        <div class="modal-body">
+                            <a href="{{url('http://www.enba.sep.gob.mx/files/calendario2015-2016.pdf')}}" target="_blank">
+                            {{ HTML::image('imagenes/mediateca/tsecundaria/calendarioEscolar.jpg','calendario',['width'=>'870','height'=>'620']) }}
+                            </a>
                         </div>
                     </div>
                 </div>
-            </div> <!-- Fin modal -->
+            </div> <!-- Fin modal calendario escolar-->
         </div>
     </div> <!-- /#wrapper -->
 </div>
