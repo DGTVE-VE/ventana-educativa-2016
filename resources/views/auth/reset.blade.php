ab212c9abf1c6b1b82c410e3cdfe3b590bf0841b
Sonia Martinez <soniamartinezctr@gmail.com>
2016-04-15 10:37:10 -0500

    cambio a vista de reseteo de contraseña

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,38 +1,38 @@
-@extends('indexVentana')
 @section('titleVentana')
 Ventana Educativa
 @stop
-
+@extends('indexVentana')
 @section('menuVentana')
 @include('viewVentana.encabezadoVentana')
 @endsection
 @section('cuerpoVentana')
-<div class="container">
-    <div class="row">
-        <div class="col-md-offset-3 col-md-8">
-            <br><br><br><br><br>
-            <form method="POST" action="{{url('password/reset')}}">
-                {{csrf_field()}}
-                <input type="hidden" name="token" value="{{$token}}" />
-                <div class="form-group">
-                    <label for="email">Correo</label>
-                    <input type="email" class="form-control" name="email" value="">        
-                    <div class="text-danger">{{$errors->first ('email')}}</div>
-                </div>
-                <div class="form-group">
-                    <label for="password">Contraseña:</label>
-                    <input type="password" class="form-control" name="password" >
-                    <div class="text-danger">{{$errors->first ('password')}}</div>
-                </div>
-                <div class="form-group">
-                    <label for="password_confirmation">Confirmar Contraseña:</label>
-                    <input type="password" class="form-control" name="password_confirmation" >        
-                </div>
-                <button type="submit" class="btn btn-success">
-                    Modificar contraseña
-                </button>
-            </form>
+<div class = "panel panel-default frmRegistro col-lg-offset-2 col-md-8">
+    <br><br>
+    <br><br>
+    <br><br>
+    <br><br>
+    <form method="POST" action="{{url('password/reset')}}">
+        {{csrf_field()}}
+        <input type="hidden" name="token" value="{{$token}}" />
+        <div class="form-group">
+            <label for="email">Correo</label>
+            <input type="email" class="form-control" name="email" value="">        
+            <div class="text-danger">{{$errors->first ('email')}}</div>
         </div>
-    </div>
+        <div class="form-group">
+            <label for="password">Contraseña:</label>
+            <input type="password" class="form-control" name="password" >
+            <div class="text-danger">{{$errors->first ('password')}}</div>
+        </div>
+        <div class="form-group">
+            <label for="password_confirmation">Confirmar Contraseña:</label>
+            <input type="password" class="form-control" name="password_confirmation" >        
+        </div>
+        <button type="submit" class="btn btn-success">
+            Modificar contraseña
+        </button>
+    </form>
+    <br><br><br><br>
+    <br><br>
 </div>
 @endsection
