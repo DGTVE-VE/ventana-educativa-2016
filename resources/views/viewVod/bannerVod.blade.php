31bfba94d4d05d50499c2efbd2a4c3fe7ee6d74e
EDA1 <EDA1>
2016-05-04 17:32:27 -0500

    Página VOD. Modificada la barra de menú de ventana. Se ocultaron los elementos de la sección de búsqueda. Se agregó el efecto de cambio de color de fondo en la barra de menú, al hacer scroll de la ventana.

--- resources/views/viewVod/bannerVod.blade.php
+++ resources/views/viewVod/bannerVod.blade.php
@@ -1,3 +1,3 @@
-<div class="container-fluid">
+<div class="container-fluid" style="height:1000px;">
     <img class="bannerVod"  src="{{asset('imagenes/vod/series/ciudad_de_los_dioses.png')}}">  
 </div>
--- resources/views/viewVod/vod.blade.php
+++ resources/views/viewVod/vod.blade.php
@@ -4,6 +4,20 @@ Videos Bajo Demanda
 @extends('indexVod')
 @section('menuVod')
 	@include('viewVentana.encabezadoVentana')
+	<script>
+		$('#buscar').css('display','none');
+		$('#iconoBuscar').css('display','none');
+		$('#navegacionVentana').css('height','55px');
+		$(window).scroll(function(){
+			var scroll_v = this.pageYOffset;
+			if(scroll_v > 10){
+				$('#navegacionVentana').css('background','rgba(0, 0, 0, .3)');
+			}
+			if(scroll_v < 10){
+				$('#navegacionVentana').css('background','transparent');
+			}
+		});
+	</script>
 @endsection
 @section('cuerpoVod')
         @include('viewVod.bannerVod')
