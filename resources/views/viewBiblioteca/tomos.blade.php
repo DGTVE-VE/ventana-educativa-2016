72205d0104720b84ede9027e38504ca767b8d49c
Sonia Martinez <soniamartinezctr@gmail.com>
2016-05-13 16:58:33 -0500

    cambio de menu para los tomos de biblioteca

--- resources/views/viewBiblioteca/tomos.blade.php
+++ resources/views/viewBiblioteca/tomos.blade.php
@@ -9,6 +9,9 @@ Biblioteca
     $('#buscar').css('display', 'none');
     $('#iconoBuscar').css('display', 'none');
     $('#navegacionVentana').css('height', '55px');
+	$('.imgLogo').attr('src','{{url("imagenes/biblioteca/imgMenu/ventanaEducativa.png")}}');
+	$('.appsLogo').attr('src','{{url("imagenes/biblioteca/imgMenu/menu.png")}}');
+	$('#img-usuario').attr('src','{{url("imagenes/biblioteca/imgMenu/registro.png")}}');
     $(window).scroll(function () {
         var scroll_v = this.pageYOffset;
         if (scroll_v > 10) {
