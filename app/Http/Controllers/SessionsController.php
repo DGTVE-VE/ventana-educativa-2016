93396fcadd787ca91cbb7115c16723c7d46ea7ca
Israel Toledo <j.israel.toledo@gmail.com>
2016-05-26 13:20:14 -0500

    Pruebas para obtener la IP del cliente

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,34 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 
-class SessionsController extends Controller
-{
-    public function create (){
-        return Redirect::to ('acceso');
+class SessionsController extends Controller {
+
+    function get_client_ip_server() {
+        $ipaddress = '';
+        if ($_SERVER['HTTP_CLIENT_IP'])
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if ($_SERVER['HTTP_X_FORWARDED_FOR'])
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if ($_SERVER['HTTP_X_FORWARDED'])
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if ($_SERVER['HTTP_FORWARDED_FOR'])
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if ($_SERVER['HTTP_FORWARDED'])
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if ($_SERVER['REMOTE_ADDR'])
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+
+        return $ipaddress;
     }
-    
-    public function store (Request $request){
-        if (Auth::attempt(Input::only ('email', 'password'))){                        
+
+    public function create() {
+        print $_SERVER['HTTP_USER_AGENT'];
+        print '<br>';
+        print $this->get_client_ip_server();
+//        return Redirect::to ('acceso');
+    }
+
+    public function store(Request $request) {
+
+        if (Auth::attempt(Input::only('email', 'password'))) {
 //            return Redirect::back();
-            $url = $request->session()->get('url', '/');           
-//            var_dump($url);
+            $url = $request->session()->get('url', '/');
             return redirect()->intended($url);
         }
-        return Redirect::back()->withInput ();
-        
+        return Redirect::back()->withInput();
     }
-    
-    public function destroy (){
-        Auth::logout ();
-        return Redirect::back ();
+
+    public function destroy() {
+        Auth::logout();
+        return Redirect::back();
     }
-    
+
 }
