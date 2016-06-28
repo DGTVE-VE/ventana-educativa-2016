b9d3130be39c356c992d4f81d79e36adb7ec3452
Sonia Martinez <soniamartinezctr@gmail.com>
2016-06-28 17:39:37 -0500

    Cambio en el estilo de admin... agregar autenticación a menú

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,8 +9,18 @@ use App\Http\Requests;
 class AdminController extends Controller
 {
     public function admin() {
+        /*Usuarios totales*/
         $usuarioTotal = DB::table('users')->count();
-//        print $usuarioTotal;
-        return view('viewAdmin/admin')->with('usuarioTotal', $usuarioTotal);
+        /*Series en educamedia*/
+        $seriesLista = DB::table('edu_serie')->select('titulo_serie')->get();
+        $videosTotalTelesecundaria = DB::table('med_telesecundaria')->count();
+        $videosTotalTelebachillerato = DB::table('med_telebachillerato')->count();        
+        return view('viewAdmin/admin')
+                ->with('usuarioTotal', $usuarioTotal)
+                ->with('seriesLista',$seriesLista)
+                ->with('videosTotalTelesecundaria',$videosTotalTelesecundaria)
+                ->with('videosTotalTelebachillerato',$videosTotalTelebachillerato)
+                ;
     }
 }
+
