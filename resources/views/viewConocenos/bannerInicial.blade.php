0da0a28159994d172864cf5f91e67f702d72739a
crapaud <renecrapaud@gmail.com>
2016-07-22 11:59:22 -0500

    Página conocenos. Pruebas de efecto de saltos de la flecha de avance en sección inicial.

--- resources/views/viewConocenos/bannerInicial.blade.php
+++ resources/views/viewConocenos/bannerInicial.blade.php
@@ -9,7 +9,7 @@
 	<!-- Behavioral Meta Data -->
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 	<!-- Styles -->
 	<link rel="stylesheet" type="text/css" href="js/parallaxCapas/examples/styles/styles.css"/>
 
@@ -26,15 +26,39 @@
 			<li class="layer" data-depth="0.00"><img src="imagenes/ventana/parallaxCapas/layer6.png"></li>
 		</ul>
 	</div>
-
+	<div class="container">
+		<div style="height:200px;">
+			<img id="flechaBrinca" src="imagenes/educaplay/flechaDetalle.png" style="width:100px; height:30px; position: absolute;"/>
+		</div>
+	</div>
 	<!-- Scripts -->
 	<script src="js/parallaxCapas/source/parallax.js"></script>
 	<script>
 
-	// Pretty simple huh?
+	// *************	Activa parallax		****************
 	var scene = document.getElementById('scene');
 	var parallax = new Parallax(scene);
-
+	
+	//	************	Efecto de salto de la flecha 	***********
+	function mueveFlecha(dir){
+		if(dir=='abajo'){
+			tiempoEspera="400";
+			desplaza = "+=20px";
+			dir = 'arriba';
+		}
+		else{
+			tiempoEspera="1200";
+			desplaza = "-=20px";
+			dir = 'abajo';
+		}
+		$('#flechaBrinca').animate({ "top": desplaza }, "slow");
+		
+		setTimeout(function(){ mueveFlecha(dir); }, tiempoEspera);
+	}
+	$( document ).ready(function() {
+		mueveFlecha('abajo');
+	});
+	
 	</script>
 
 </body>
