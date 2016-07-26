81e46fa098f0fc4d7eb66d2a5ed9da90c82a5383
crapaud <renecrapaud@gmail.com>
2016-07-26 17:41:21 -0500

    Ventana educativa. Sección conocenos. Agregado el efecto de cambio de imagenes de descripciones al hacer scroll en sección inicial. Agregada la imagen de flecha hacia abajo con el efecto de brinco.

--- resources/views/viewConocenos/cuerpo.blade.php
+++ resources/views/viewConocenos/cuerpo.blade.php
@@ -2,21 +2,73 @@
 <script type="text/javascript" src="js/fullPage/vendors/scrolloverflow.min.js"></script>
 <script type="text/javascript" src="js/fullPage/jquery.fullPage.js"></script>
 <script>
-$(document).ready(function() {
-    $('#fullpage').fullpage(
-		{navigation:true, scrollOverflow:true, loopTop:true, 
-			onLeave: 
-			function(index, nextIndex, direction){
-				var leavingSection = $(this);
-				if(index == 1 && nextIndex == 4){
-					return false;
-				}
-				else if(index == 1 && nextIndex == 2){
+	$(document).ready(function() {
+	//	************	Activación de elemento fullPage (desplazamiento vertical) 	***********
+		$('#fullpage').fullpage(
+			{navigation:true, scrollOverflow:true, loopTop:true, 
+				onLeave: 
+				function(index, nextIndex, direction){
+					var leavingSection = $(this);
+					if(index == 1 && nextIndex == 4){
+						if($('#textoProyectos').css('display') == 'none'){
+							muestraProyectos();
+							return false;
+						}
+						else{
+							return false;
+						}
+					}
+					else if(index == 1 && nextIndex == 2){
+						if($('#textoObjetivos').css('display') == 'none'){
+							muestraObjetivos();
+							return false;
+						}
+					}
 				}
 			}
+		);
+	});
+
+	//	************	Efecto de salto de la flecha 	***********
+	function mueveFlecha(dir){
+		if(dir=='abajo'){
+			tiempoEspera="400";
+			desplaza = "+=20px";
+			dir = 'arriba';
 		}
-	);
-});
+		else{
+			tiempoEspera="1200";
+			desplaza = "-=20px";
+			dir = 'abajo';
+		}
+		$('#flechaBrinca').animate({ "top": desplaza }, "slow");
+		
+		setTimeout(function(){ mueveFlecha(dir); }, tiempoEspera);
+	}
+	$( document ).ready(function() {
+		mueveFlecha('abajo');
+	});
+	
+	function muestraProyectos(){
+		$('#textoObjetivos').removeClass('slideInDown');
+		$('#textoObjetivos').addClass('slideOutUp');
+		$('#textoObjetivos').addClass('animated');
+		$('#textoObjetivos').addClass('oculta');
+		$('#textoObjetivos').removeClass('animated');
+		$('#textoProyectos').removeClass('slideOutDown');
+		$('#textoProyectos').addClass('slideInUp');
+		$('#textoProyectos').addClass('animated');
+		$('#textoProyectos').removeClass('oculta');
+	}
+	
+	function muestraObjetivos(){
+		$('#textoProyectos').removeClass('slideInUp');
+		$('#textoProyectos').addClass('slideOutDown');
+		$('#textoProyectos').addClass('oculta');
+		$('#textoObjetivos').removeClass('oculta');
+		$('#textoObjetivos').removeClass('slideOutUp');
+		$('#textoObjetivos').addClass('slideInDown');
+	}
 </script>
 <style>
 	.fondo1{
@@ -34,11 +86,21 @@ $(document).ready(function() {
 	.oculta{
 		display: none;
 	}
+	.flechaBrinca{
+		width:60px; height:18px; position: absolute; cursor:pointer;
+	}
+	.divFlechaBrinca{
+		position: absolute; bottom: 50px; text-align: center;
+	}
 </style>
 <div id="fullpage">
     <div class="section fondo1">
-		<img id="textoObjetivos" class="animated" src="imagenes/ventana/conocenos/objetivos.png"/>
-		<img id="textoProyecto" class="oculta animated" src="imagenes/ventana/conocenos/proyecto.png"/>
+		<img id="textoObjetivos" class="oculta animated" src="imagenes/ventana/conocenos/objetivos.png"/>
+		<img id="textoProyectos" class="animated" src="imagenes/ventana/conocenos/proyecto.png"/>
+		<div style="" class="divFlechaBrinca">
+			SCROLL DOWN
+			<img id="flechaBrinca" class="flechaBrinca" src="imagenes/educaplay/flechaDetalle.png"/>
+		</div>
 	</div>
     <div class="section fondo2">Some section 2</div>
     <div class="section fondo3">Some section 3</div>
