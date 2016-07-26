5e8fc47134a71e265765dc2f20635bc4287ed97a
crapaud <renecrapaud@gmail.com>
2016-07-26 13:37:04 -0500

    Ventana educativa. Sección conocenos. Agregada la biblioteca de javaScript de fullPage, que agrega estilos de css para efectos de desplazamiento vertical en la página. Sección conocenos. Modificada la vista inicial, agregada la estructura para acomodar las secciones verticalmente en la página. Sección conocenos. Agregadas las imagenes y videos de prueba para la primera sección de la vista.

--- resources/views/viewConocenos/cuerpo.blade.php
+++ resources/views/viewConocenos/cuerpo.blade.php
@@ -1,57 +1,49 @@
-<div id="wrapper">
-    <div class="overlay"></div>
-
-    <!-- Sidebar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
-        <ul class="nav sidebar-nav">
-            <li class="sidebar-brand">
-                <a href="#">
-                    Conocenos
-                </a>
-            </li>
-            <li>
-                <a href="#">Blog</a>
-            </li>
-            <li>
-                <a href="#">Acerca de</a>
-            </li>
-        </ul>
-    </nav>
-    <!-- /#sidebar-wrapper -->
-
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-        <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
-            <span class="hamb-top"></span>
-            <span class="hamb-middle"></span>
-            <span class="hamb-bottom"></span>
-        </button>
-        <div class="container">
-            <div class="row">
-                <div class="menuFloat">
-                    <a href="#el1" class="click"><div class="full-circle"></div></a><label id="scrollLabel1" class="scrollLabel">Cultura</label>
-                    <a href="#el2" class="click"><div class="full-circle"></div></a><label id="scrollLabel2" class="scrollLabel">Canal Iberoamericano</label>
-                    <a href="#el3" class="click"><div class="full-circle"></div></a><label id="scrollLabel3" class="scrollLabel">Educaplay</label>
-                    <a href="#el4" class="click"><div class="full-circle"></div></a><label id="scrollLabel4" class="scrollLabel">RedMITE</label>
-                    <a href="#el5" class="click"><div class="full-circle"></div></a><label id="scrollLabel5" class="scrollLabel">Educamedia</label>
-                    <a href="#el6" class="click"><div class="full-circle"></div></a><label id="scrollLabel6" class="scrollLabel">Biblioteca</label>
-                    <a href="#el7" class="click"><div class="full-circle"></div></a><label id="scrollLabel7" class="scrollLabel">MéxicoX</label>
-                    <a href="#el8" class="click"><div class="full-circle"></div></a><label id="scrollLabel8" class="scrollLabel">Salud</label>
-                    <a href="#el9" class="click"><div class="full-circle"></div></a><label id="scrollLabel9" class="scrollLabel">Contacto</label>
-                </div>
-
-                <div class="divScroll" id="el1">Cultura</div>
-                <div class="divScroll" id="el2">Canal Iberoamericano</div>
-                <div class="divScroll" id="el3">Educaplay</div>
-                <div class="divScroll" id="el4">RedMITE</div>
-                <div class="divScroll" id="el5">Educamedia</div>
-                <div class="divScroll" id="el6">Biblioteca</div>
-                <div class="divScroll" id="el7">MéxicoX</div>
-                <div class="divScroll" id="el8">Salud</div>
-                <div class="divScroll" id="el9">Cotacto</div>
-            </div>            
-        </div>
-        <!-- /#page-content-wrapper -->       
-        <div class="scrollup"></div><!-- /div botón scroll -->
-    </div>
-    <!-- /#wrapper -->
+<link rel="stylesheet" type="text/css" href="js/fullPage/jquery.fullPage.css"/>
+<script type="text/javascript" src="js/fullPage/vendors/scrolloverflow.min.js"></script>
+<script type="text/javascript" src="js/fullPage/jquery.fullPage.js"></script>
+<script>
+$(document).ready(function() {
+    $('#fullpage').fullpage(
+		{navigation:true, scrollOverflow:true, loopTop:true, 
+			onLeave: 
+			function(index, nextIndex, direction){
+				var leavingSection = $(this);
+				if(index == 1 && nextIndex == 4){
+					return false;
+				}
+				else if(index == 1 && nextIndex == 2){
+				}
+			}
+		}
+	);
+});
+</script>
+<style>
+	.fondo1{
+		background-color:gray;
+	}
+	.fondo2{
+		background:#00ff80;
+	}
+	.fondo3{
+		background:#79738c;
+	}
+	.fondo4{
+		background:#bf4060;
+	}
+	.oculta{
+		display: none;
+	}
+</style>
+<div id="fullpage">
+    <div class="section fondo1">
+		<img id="textoObjetivos" class="animated" src="imagenes/ventana/conocenos/objetivos.png"/>
+		<img id="textoProyecto" class="oculta animated" src="imagenes/ventana/conocenos/proyecto.png"/>
+	</div>
+    <div class="section fondo2">Some section 2</div>
+    <div class="section fondo3">Some section 3</div>
+    <div class="section fondo4"> <div class="slide"> Slide 1 </div>
+		<div class="slide"> Slide 2 </div>
+		<div class="slide"> Slide 3 </div>
+		<div class="slide"> Slide 4 </div></div>
+</div>
