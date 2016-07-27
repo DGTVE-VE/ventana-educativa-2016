fcc7f88d156242d4d5769823f71272422489cc45
crapaud <renecrapaud@gmail.com>
2016-07-27 12:51:19 -0500

    Ventana educativa. Sección conocenos. Agregado el efecto de entrada y salida de las imagenes con textos descriptivos de proyectos y objetivos.

--- resources/views/viewConocenos/cuerpo.blade.php
+++ resources/views/viewConocenos/cuerpo.blade.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" type="text/css" href="js/fullPage/jquery.fullPage.css"/>
 <script type="text/javascript" src="js/fullPage/vendors/scrolloverflow.min.js"></script>
 <script type="text/javascript" src="js/fullPage/jquery.fullPage.js"></script>
+<link rel="stylesheet" type="text/css" href="css/red/animate.css"/>
 <script>
 	$(document).ready(function() {
 	//	************	Activación de elemento fullPage (desplazamiento vertical) 	***********
@@ -48,26 +49,26 @@
 	$( document ).ready(function() {
 		mueveFlecha('abajo');
 	});
-	
-	function muestraProyectos(){
-		$('#textoObjetivos').removeClass('slideInDown');
-		$('#textoObjetivos').addClass('slideOutUp');
-		$('#textoObjetivos').addClass('animated');
-		$('#textoObjetivos').addClass('oculta');
-		$('#textoObjetivos').removeClass('animated');
-		$('#textoProyectos').removeClass('slideOutDown');
-		$('#textoProyectos').addClass('slideInUp');
-		$('#textoProyectos').addClass('animated');
-		$('#textoProyectos').removeClass('oculta');
-	}
-	
+
 	function muestraObjetivos(){
-		$('#textoProyectos').removeClass('slideInUp');
-		$('#textoProyectos').addClass('slideOutDown');
-		$('#textoProyectos').addClass('oculta');
-		$('#textoObjetivos').removeClass('oculta');
-		$('#textoObjetivos').removeClass('slideOutUp');
-		$('#textoObjetivos').addClass('slideInDown');
+		$('#textoObjetivos').removeClass('slideOutDown');
+		$('#textoProyectos').removeClass('slideInDown');
+		$('#textoProyectos').addClass('slideOutUp');
+		setTimeout(function(){ 
+			$('#textoProyectos').addClass('oculta');
+			$('#textoObjetivos').removeClass('oculta');
+			$('#textoObjetivos').addClass('slideInUp'); 
+		}, 500);
+	}
+
+	function muestraProyectos(){
+		$('#textoProyectos').removeClass('slideOutUp');
+		$('#textoObjetivos').removeClass('slideInUp');
+		$('#textoObjetivos').addClass('slideOutDown');
+		setTimeout(function(){ $('#textoObjetivos').addClass('oculta');
+			$('#textoProyectos').removeClass('oculta');
+			$('#textoProyectos').addClass('slideInDown');
+		}, 500);
 	}
 </script>
 <style>
@@ -93,19 +94,50 @@
 		position: absolute; bottom: 50px; text-align: center;
 	}
 </style>
-<div id="fullpage">
-    <div class="section fondo1">
-		<img id="textoObjetivos" class="oculta animated" src="imagenes/ventana/conocenos/objetivos.png"/>
-		<img id="textoProyectos" class="animated" src="imagenes/ventana/conocenos/proyecto.png"/>
-		<div style="" class="divFlechaBrinca">
-			SCROLL DOWN
-			<img id="flechaBrinca" class="flechaBrinca" src="imagenes/educaplay/flechaDetalle.png"/>
-		</div>
-	</div>
-    <div class="section fondo2">Some section 2</div>
-    <div class="section fondo3">Some section 3</div>
-    <div class="section fondo4"> <div class="slide"> Slide 1 </div>
-		<div class="slide"> Slide 2 </div>
-		<div class="slide"> Slide 3 </div>
-		<div class="slide"> Slide 4 </div></div>
+
+<div id="wrapperConocenos">
+    <div class="overlayConocenos"></div>
+
+    <!-- Sidebar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapperConocenos" role="navigation">
+        <ul class="nav sidebar-nav">
+            <h4 class="etiquetaMenu">MENÚ</h4>
+            <li>
+                <a href="#">BLOG</a>
+            </li>
+            <li>
+                <a href="#">ACERCA DE</a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapperConocenos">
+        <button type="button" class="hamburgerConocenos is-closed" data-toggle="offcanvas">
+            <span class="hamb-top"></span>
+            <span class="hamb-middle"></span>
+            <span class="hamb-bottom"></span>
+        </button>
+        <div class="container-fluid">
+			<div id="fullpage">
+				<div class="section fondo1">
+					<img id="textoObjetivos" class="oculta animated" src="imagenes/ventana/conocenos/objetivos.png"/>
+					<img id="textoProyectos" class="animated" src="imagenes/ventana/conocenos/proyecto.png"/>
+					<div style="" class="divFlechaBrinca">
+						SCROLL DOWN
+						<img id="flechaBrinca" class="flechaBrinca" src="imagenes/educaplay/flechaDetalle.png"/>
+					</div>
+				</div>
+				<div class="section fondo2">Some section 2</div>
+				<div class="section fondo3">Some section 3</div>
+				<div class="section fondo4"> <div class="slide"> Slide 1 </div>
+					<div class="slide"> Slide 2 </div>
+					<div class="slide"> Slide 3 </div>
+					<div class="slide"> Slide 4 </div></div>
+			</div>
+        </div>
+        <!-- /#wrapper -->
+    </div>
 </div>
+    <script src="https://cdn.jsdelivr.net/jquery.fullpage/2.5.9/jquery.fullPage.min.js"></script>
