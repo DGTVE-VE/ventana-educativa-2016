6282a508b9f49280801005a69795719ab15650b6
crapaud <renecrapaud@gmail.com>
2016-08-01 15:09:29 -0500

    Sección conocenos. Agregadas a la vista las figuras en las que se verán las trancisiones. Agregadas las clases de estilos y efectos con css.

--- resources/views/viewConocenos/slider.blade.php
+++ resources/views/viewConocenos/slider.blade.php
@@ -25,24 +25,62 @@ Conocenos
                         <div class="item">
                             <div class="profile-circle" style="background-color: rgba(0,0,0,.2);"></div>
                             <blockquote>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+                                <p>Aqui va el contenido de colaboradores.</p>
                             </blockquote>	
                         </div>
-                        <div class="item">
+                        <div class="active item">
                             <div class="profile-circle" style="background-color: rgba(77,5,51,.2);"></div>
                             <blockquote>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+                                <p>Aqui va el contenido de la sección antecedentes.</p>
                             </blockquote>
                         </div>
-                        <div class="active item">
+                        <div class="item">
                             <div class="profile-circle" style="background-color: rgba(145,169,216,.2);"></div>
                             <blockquote>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>
+                                <p>Aqui va el contenido de alineación del proyecto.</p>
                             </blockquote>
                         </div>
                     </div>                    
                 </div>
-            </div>							
+            </div>
+			<style>
+				#target{
+					width:300px;
+					height:300px;
+					border-radius:0%;
+					/*transition: border-radius 0.8s ease;*/
+					background-color: purple;
+					transition-duration: 1s;
+				}
+				.circulo{
+					border-radius: 50% !important;
+				}
+				.trianguloArriba{
+					width:0;
+					height:0;
+					border-left:150px solid transparent !important;
+					border-right:150px solid transparent !important;
+					border-bottom:300px solid purple !important;;
+				}
+			</style>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+			<script>
+				function tranformaFigura(){
+					if($('#target').hasClass('trianguloArriba')){
+						$('#target').css({transform:'rotate(90deg)'});
+						$('#target').removeClass('trianguloArriba');
+					}else{
+						$('#target').css({transform:'rotate(-90deg)'});
+						$('#target').addClass('trianguloArriba');
+					}
+					
+				}
+			</script>
+			<div class="col-md-12">
+				<div id="target" onclick="tranformaFigura()"></div>
+				<!//div class="trianguloArriba" onclick="tranformaFigura()"></div-->
+			</div>
         </div>
     </div>
 </section>
