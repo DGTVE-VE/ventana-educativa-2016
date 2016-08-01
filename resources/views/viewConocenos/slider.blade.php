0fb39f10cc68adf6ed4945ba66bd26a6d3c0bd09
crapaud <renecrapaud@gmail.com>
2016-08-01 17:14:07 -0500

    Vista conocenos, segunda sección. Agregados los efectos de trancisión de las figuras cuando se cambia de una diapositiva a otra.

--- resources/views/viewConocenos/slider.blade.php
+++ resources/views/viewConocenos/slider.blade.php
@@ -12,11 +12,11 @@ Conocenos
                     <div class="col-md-12">
                         <!-- Carousel indicators -->
                         <div class="col-md-2"></div>
-                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="0"> <a class="first after" href="#">Colaboradores</a></div>
+                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="0"> <a class="first after" href="#" onclick="tranformaFigura(1)">Colaboradores</a></div>
                         <div class="col-md-1"></div>
-                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="1"> <a class="first after" href="#">Antecedentes</a></div>
+                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="1"> <a class="first after" href="#" onclick="tranformaFigura(2)">Antecedentes</a></div>
                         <div class="col-md-1"></div>
-                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="2"> <a class="first after" href="#">Alineación del Proyecto</a></div>
+                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="2"> <a class="first after" href="#" onclick="tranformaFigura(3)">Alineación del Proyecto</a></div>
                         <div class="col-md-2"></div>                        
                     </div>
                     
@@ -44,7 +44,7 @@ Conocenos
                 </div>
             </div>
 			<style>
-				#target{
+				#divFigura{
 					width:300px;
 					height:300px;
 					border-radius:0%;
@@ -54,33 +54,71 @@ Conocenos
 				}
 				.circulo{
 					border-radius: 50% !important;
+					transition-duration: 1s;
 				}
 				.trianguloArriba{
-					width:0;
-					height:0;
+					width:0 !important;
+					height:0 !important;
 					border-left:150px solid transparent !important;
 					border-right:150px solid transparent !important;
-					border-bottom:300px solid purple !important;;
+					border-bottom:300px solid purple !important;
+					transition-duration: 1s;
 				}
 			</style>
+			<div class="col-md-12">
+				<div style="position:absolute;" id="divFigura"></div>
+				<div style="position:absolute;" id="divTriangulo" class="trianguloArriba"></div>
+			</div>
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
 			<script>
-				function tranformaFigura(){
-					if($('#target').hasClass('trianguloArriba')){
-						$('#target').css({transform:'rotate(90deg)'});
-						$('#target').removeClass('trianguloArriba');
-					}else{
-						$('#target').css({transform:'rotate(-90deg)'});
-						$('#target').addClass('trianguloArriba');
+				$('#divTriangulo').css({transform:'scale(0.1, 0.1)'});
+				actual = 2;
+				function tranformaFigura(seccion){
+					switch(seccion){
+						case 1:	//circulo
+							if(actual==2){
+								$('#divFigura').css({transform:'rotate(-90deg)'});
+								$('#divFigura').addClass('circulo');
+							}else if(actual==3){
+								if(!$('#divFigura').hasClass('circulo')){
+									$('#divFigura').addClass('circulo');
+								}
+								$('#divFigura').css({transform:'rotate(90deg)'});
+								//$('#divFigura').css({transform:'scale(1, 1)'});
+								$('#divTriangulo').css({transform:'rotate(-90deg)'});
+								$('#divTriangulo').css({transform:'scale(0.1, 0.1)'});
+							}
+							actual =1;
+							break;
+						case 2:	//cuadrado
+							if(actual==1){
+								$('#divFigura').css({transform:'rotate(90deg)'});
+								$('#divFigura').removeClass('circulo');
+							}else if(actual==3){
+								if($('#divFigura').hasClass('circulo')){
+									$('#divFigura').removeClass('circulo');
+								}
+								$('#divFigura').css({transform:'rotate(90deg)'});
+								//$('#divFigura').css({transform:'scale(1, 1)'});
+								$('#divTriangulo').css({transform:'rotate(-90deg)'});
+								$('#divTriangulo').css({transform:'scale(0.1, 0.1)'});
+							}
+							actual =2;
+							break;
+						case 3:	//triangulo
+							if(actual==1 || actual==2){
+								$('#divTriangulo').css({transform:'rotate(-90deg)'});
+								//$('#divTriangulo').css({transform:'scale(1, 1)'});
+								$('#divFigura').css({transform:'rotate(-90deg)'});
+								$('#divFigura').css({transform:'scale(0.1, 0.1)'});
+							}
+							actual =3;
+							break;
+						default:
+							break;
 					}
-					
 				}
 			</script>
-			<div class="col-md-12">
-				<div id="target" onclick="tranformaFigura()"></div>
-				<!//div class="trianguloArriba" onclick="tranformaFigura()"></div-->
-			</div>
         </div>
     </div>
 </section>
