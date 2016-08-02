b84f37f3d53b920c964a353cced9b47e6bdfaafa
crapaud <renecrapaud@gmail.com>
2016-08-02 14:05:40 -0500

    Vista conocenos. Segunda sección. Ajuste de efectos de transición de figuras de fondo. Agregada imagen de fondo que se moverá junto con el puntero. Agregada la librería javascript y activado el efecto de movimiento de la imagen de fondo al activarse el puntero.

--- resources/views/viewConocenos/slider.blade.php
+++ resources/views/viewConocenos/slider.blade.php
@@ -3,29 +3,42 @@ Conocenos
 @stop
 @extends('indexConocenos')
 @section('cuerpoConocenos')
-<section id="carousel">    				
+<!--	********	Parallax Capas	***********	-->
+<!-- Behavioral Meta Data -->
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<!-- Styles -->
+<!--link rel="stylesheet" type="text/css" href="js/parallaxCapas/examples/styles/styles.css"/-->
+	<div class="container" style="position:absolute;">
+		<ul id="scene" class="scene">
+			<li class="layer" data-depth="0.80"><img src="js/parallaxCapas/examples/images/layer2.png" style="height:70%; width:80%;"></li>
+		</ul>
+	</div>
+
+<section id="carousel">
     <div class="container">
         <div class="row">
 			<div class="col-md-8 col-md-offset-4">
-				<div style="position:absolute; " id="divFigura"></div>
+				<div style="position:absolute; top:70px; left:-80px; border: solid 3px; width:500px; height:180px;"></div>
 				<div style="position:absolute;" id="divTriangulo" class="trianguloArriba"></div>
+				<div style="position:absolute; " id="divFigura"></div>
 			</div>
-            <div class="col-md-12" style="position:relative; top:80px;">
+            <div class="col-md-12" style="position:relative; top:100px;">
                 <!--<div class="quote"><i class="fa fa-quote-left fa-4x"></i></div>-->
-                <div class="carousel slide" id="fade-quote-carousel" data-ride="carousel" data-interval="0" style="border: solid 3px;">
+                <div class="carousel slide" id="fade-quote-carousel" data-ride="carousel" data-interval="0">
                     <div class="col-md-12">
                         <!-- Carousel indicators -->
-                        <div class="col-md-2"></div>
-                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="0"> <a class="first after" href="#" onclick="tranformaFigura(1)">Colaboradores</a></div>
                         <div class="col-md-1"></div>
-                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="1"> <a class="first after" href="#" onclick="tranformaFigura(2)">Antecedentes</a></div>
+                        <div class=" col-md-3" data-target="#fade-quote-carousel" data-slide-to="0"> <a class="first after" href="#" onclick="tranformaFigura(1)">Colaboradores</a></div>
                         <div class="col-md-1"></div>
-                        <div class="botonConocenosSlide col-md-2" data-target="#fade-quote-carousel" data-slide-to="2"> <a class="first after" href="#" onclick="tranformaFigura(3)">Alineación del Proyecto</a></div>
-                        <div class="col-md-2"></div>                        
+                        <div class=" col-md-3" data-target="#fade-quote-carousel" data-slide-to="1"> <a class="first after" href="#" onclick="tranformaFigura(2)">Antecedentes</a></div>
+                        <div class="col-md-1"></div>
+                        <div class=" col-md-3" data-target="#fade-quote-carousel" data-slide-to="2"> <a class="first after" href="#" onclick="tranformaFigura(3)">Alineación del Proyecto</a></div>
+                        <!--div class="col-md-1"></div-->
                     </div>
                     
                     <!-- Carousel items -->
-                    <div class="carousel-inner" style="position: relative; top:200px;">
+                    <div class="carousel-inner" style="position: relative; top:220px;">
                         <div class="item">
                             <!--div class="profile-circle" style="background-color: rgba(0,0,0,.2);"></div-->
                             <blockquote>
@@ -52,7 +65,11 @@ Conocenos
 					width:300px;
 					height:300px;
 					border-radius:0%;
-					background-color: purple;
+					background: #33ffff; /* For browsers that do not support gradients */
+					background: -webkit-linear-gradient(#33ffff, #ccffff); /* For Safari 5.1 to 6.0 */
+					background: -o-linear-gradient(#33ffff, #ccffff); /* For Opera 11.1 to 12.0 */
+					background: -moz-linear-gradient(#33ffff, #ccffff); /* For Firefox 3.6 to 15 */
+					background: linear-gradient(#33ffff, #ccffff); /* Standard syntax */
 					transition-duration: 0.8s;
 				}
 				.circulo{
@@ -64,11 +81,14 @@ Conocenos
 					height:0 !important;
 					border-left:150px solid transparent !important;
 					border-right:150px solid transparent !important;
-					border-bottom:300px solid purple !important;
+					border-bottom:300px solid #33ffff !important;
 					transition-duration: 0.8s;
 				}
 			</style>
+			<!--	********* Incluir Jquery	***************	-->
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+			<!--	********* Incluir libreria parallax	***************	-->
+			<script src="js/parallaxCapas/deploy/parallax.js"></script>
 			<script>
 				$('#divTriangulo').css({transform:'scale(0.1, 0.1)'});
 				actual = 2;
@@ -83,7 +103,7 @@ Conocenos
 									$('#divFigura').addClass('circulo');
 								}
 								$('#divFigura').css({transform:'rotate(-90deg)'});
-								$('#divTriangulo').css({transform:'rotate(-90deg) scale(0.1, 0.1)'});
+								$('#divTriangulo').css({transform:'rotate(-45deg) scale(0.1, 0.1)'});
 							}
 							actual =1;
 							break;
@@ -96,14 +116,17 @@ Conocenos
 									$('#divFigura').removeClass('circulo');
 								}
 								$('#divFigura').css({transform:'rotate(-90deg)'});
-								$('#divTriangulo').css({transform:'rotate(-90deg) scale(0.1, 0.1)'});
+								$('#divTriangulo').css({transform:'rotate(-45deg) scale(0.1, 0.1)'});
 							}
 							actual =2;
 							break;
 						case 3:	//triangulo
 							if(actual==1 || actual==2){
-								$('#divTriangulo').css({transform:'rotate(90deg)'});
-								$('#divFigura').css({transform:'scale(0.1, 0.1) rotate(90deg)'});
+								$('#divTriangulo').css({transform:'rotate(60deg)'});
+								if($('#divFigura').hasClass('circulo')){
+									$('#divFigura').removeClass('circulo');
+								}
+								$('#divFigura').css({transform:'scale(0.3, 0.1) rotate(70deg)'});
 							}
 							actual =3;
 							break;
@@ -111,6 +134,10 @@ Conocenos
 							break;
 					}
 				}
+				
+				<!--	***********	Activar parallax fondo	*************	-->
+				var scene = document.getElementById('scene');
+				var parallax = new Parallax(scene);
 			</script>
         </div>
     </div>
