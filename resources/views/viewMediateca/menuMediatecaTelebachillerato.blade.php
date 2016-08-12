aad3bd357b2d0bc8ed131e9f19a9f351839141e9
Sonia Martinez <soniamartinezctr@gmail.com>
2016-08-12 16:40:43 -0500

    Integrar menú de telebachillerato a educamedia con links actualizados

--- resources/views/viewMediateca/menuMediatecaTelebachillerato.blade.php
+++ resources/views/viewMediateca/menuMediatecaTelebachillerato.blade.php
@@ -5,11 +5,11 @@
         <div class="margensuperiormenuTS">
             <nav class="navbar navbar-inverse navbar-fixed-top margensuperiormenuTS" id="sidebar-wrapperTB" role="navigation">
                 <ul class="nav sidebar-nav">
-                    <li><a href="{{url('http://www.dgb.sep.gob.mx/02-m1/02-subsistemas/telebachillerato/NORMATIVIDAD/DOCUMENTO_BASE_TBC_2015.pdf')}}" target="_blank">Normatividad</a></li>
+                    <li><a href="{{url('http://www.dgb.sep.gob.mx/servicios-educativos/telebachillerato/normatividad/DOCUMENTO_BASE_TBC_2015.pdf')}}" target="_blank">Normatividad</a></li>
                     <li><a data-toggle="modal" href="#myModal-mapaCurricular-tb">Mapa Curricular</a></li>
                     <li><a data-toggle="modal" href="#myModal-calendario-tb">Calendario</a></li>
-                    <li><a href="{{url('http://www.dgb.sep.gob.mx/02-m1/02-subsistemas/telebachillerato.php?id=materiales_audiovisuales')}}" target="_blank">Materiales Audiovisuales</a></li>
-                    <li id="sinBorde"><a href="{{url('http://www.dgb.sep.gob.mx/02-m1/02-subsistemas/telebachillerato.php?id=materiales_impresos')}}" target="_blank">Material Impreso</a></li>
+                    <li><a href="{{url('http://www.tuprepaenvideos.sep.gob.mx/')}}" target="_blank">Materiales Audiovisuales</a></li>
+                    <li id="sinBorde"><a href="{{url('http://www.dgb.sep.gob.mx/servicios-educativos/telebachillerato/materiales_impresos.php')}}" target="_blank">Material Impreso</a></li>
                 </ul>
             </nav>
 
@@ -30,8 +30,8 @@
                     <h4 class="modal-title">Telebachillerato</h4>
                 </div>
                 <div class="modal-body">
-                    <a href="{{url('http://www.dgb.sep.gob.mx/02-m1/02-subsistemas/telebachillerato/NORMATIVIDAD/Mapa_CURRICULAR_tbc_2014.pdf')}}" target="_blank">
-                        {{ HTML::image('imagenes/mediateca/tbachillerato/mapaCurriculartb.png','mapa curricular',['width'=>'100%','height'=>'100%'])}}
+                    <a href="{{url('http://www.dgb.sep.gob.mx/servicios-educativos/telebachillerato/normatividad/Mapa_CURRICULAR_tbc_2014.pdf')}}" target="_blank">
+                        {{ HTML::image('imagenes/mediateca/tbachillerato/mapa_curric_2008.gif','mapa curricular',['width'=>'100%','height'=>'100%'])}}
                     </a>
                 </div>
             </div>
