05a8f2439c931b9dd14aa09ad7a499eaa9711082
crapaud <renecrapaud@gmail.com>
2016-08-16 12:56:47 -0500

    Ventana educativa. Seccion conocenos. Apartado presentación. Modal de visualización de video. Eliminado borde inferior y botón de cerrar. Agregado en la parte de título icono de cruz para cerrar modal.

--- resources/views/viewConocenos/vistaVideo.blade.php
+++ resources/views/viewConocenos/vistaVideo.blade.php
@@ -24,15 +24,13 @@
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 					<h4 class="modal-title" id="etiquetaModalVideo">Conocenos</h4>
 				</div>
 				<div clas="modal-body">
 					<video style="width:100%;" controls>
 						<source src="imagenes/ventana/conocenos/DJI_0008.mp4" type="video/mp4"/>
 					</video>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 				</div>
 			</div>
 		</div>
