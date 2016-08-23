2d4ca2405ff1b9d2b7b078eaaf8d87a27c863298
crapaud <renecrapaud@gmail.com>
2016-08-23 18:36:34 -0500

    Sección conocenos. Apartado slider. Ajuste en posición de figuras del frente del slider y tamaños dependiendo de la resolución que se muestra.

--- resources/views/viewConocenos/slider.blade.php
+++ resources/views/viewConocenos/slider.blade.php
@@ -9,7 +9,32 @@
 	<ul id="scene2" class="scene" style="position:absolute; top:0px; left:250px; width:100%">
 		<li class="layer" data-depth="0.40"><img src="imagenes/ventana/conocenos/slider/red2.png" style="height:70%; width:90%;"/></li>
 	</ul>
-	<section id="carousel" style="position:relative; top:0px;">
+        <div class="row" style="margin:0px;">
+			<div class="col-md-4 col-md-offset-4">
+				<img class="img-responsive" id="imgMarcoNegro" src="imagenes/ventana/conocenos/slider/marcoNegro.png" style="position: relative; top:-10%; left:-2%; max-width:110%;"/>
+				<img class="img-responsive" id="imgApartadoEdificio" src="imagenes/ventana/conocenos/slider/antecedentes.png" style="position: absolute; top:-0.6%; left:1.5%; max-width:102.1%;"/>
+				<div id="divTriangulo" class="trianguloDerecha opaco homologaColor"></div>
+				<div id="divFigura" class="opaco"></div>
+			</div>
+		</div>
+        <div class="row" style="margin:0px;">
+			 <div class="col-md-4 altura50Celda" onclick="tranformaFigura(1)">
+				<a class="first after txtNegro acomodaLigas">Colaboradores</a>
+			</div>
+			<div class="col-md-4 altura50Celda" onclick="tranformaFigura(2)">
+				<a class="first after txtNegro acomodaLigas">Antecedentes</a>
+			</div>
+			<div class="col-md-4 altura50Celda" onclick="tranformaFigura(3)">
+				<a class="first after txtNegro acomodaLigas">Alineación del Proyecto</a>
+			</div>
+		</div>
+        <div class="row" style="margin:0px; position:relative; top:0px;">
+			<div class="col-md-6 col-md-offset-3">
+					<br>
+					<img class="img-responsive" src="imagenes/ventana/conocenos/slider/textoAntecedentes.png" style="margin: auto; width:90%;"/>
+			</div>
+		</div>
+	<!--section id="carousel" style="position:relative; top:0px;">
         <div class="row" style="margin:0px;">
 			<div class="col-md-4 col-md-offset-4" style="position: absolute; top:0px; height:100%;">
 				<img class="img-responsive" id="imgMarcoNegro" src="imagenes/ventana/conocenos/slider/marcoNegro.png" style="position: absolute; top:-10%; left:-5%; max-width:110%;"/>
@@ -21,19 +46,19 @@
             <div class="col-md-12" >
                 <div class="carousel" id="fade-quote-carousel" data-ride="carousel" data-interval="0">
                     <div class="col-md-12">
-                        <!-- Carousel indicators -->
-                        <div class=" col-md-4 text-center" data-target="#fade-quote-carousel" data-slide-to="0" onclick="tranformaFigura(1)">
+
+                        <div class="col-md-4 text-center" data-target="#fade-quote-carousel" data-slide-to="0" onclick="tranformaFigura(1)">
 							<a class="first after txtNegro" href="#">Colaboradores</a>
 						</div>
-                        <div class=" col-md-4 text-center" data-target="#fade-quote-carousel" data-slide-to="1" onclick="tranformaFigura(2)">
+                        <div class="col-md-4 text-center" data-target="#fade-quote-carousel" data-slide-to="1" onclick="tranformaFigura(2)">
 							<a class="first after txtNegro" href="#">Antecedentes</a>
 						</div>
-                        <div class=" col-md-4 text-center" data-target="#fade-quote-carousel" data-slide-to="2" onclick="tranformaFigura(3)">
+                        <div class="col-md-4 text-center" data-target="#fade-quote-carousel" data-slide-to="2" onclick="tranformaFigura(3)">
 							<a class="first after txtNegro" href="#">Alineación del Proyecto</a>
 						</div>
                     </div>
                     
-                    <!-- Carousel items -->
+
                     <div class="carousel-inner" style="position: relative; top:200px;">
                         <div class="item">
                             <blockquote>
@@ -57,11 +82,11 @@
                 </div>
             </div>
         </div>
-	</section>
+	</section-->
 	<style>
 		#divFigura{
-			position:absolute; top:7%; left:24%;
-			width:250px;	height:250px;
+			position:absolute; top:23%; left:30%;
+			width:240px;	height:240px;
 			border-radius:0%;
 			background: #6bebcf; /* For browsers that do not support gradients */
 			transition: visibility 0.3s, border-radius 0.8s, transform 0.8s;
@@ -88,8 +113,83 @@
 			opacity: 0.9;
 			transition: opacity:0.3s;
 		}
-		txtNegro{
+		.txtNegro{
 			color:black;
+		}
+		.acomodaLigas{
+			position: absolute; left:40%;
+		}
+		@media (min-width:992px ){
+			.acomodaLigas{
+				position: absolute; top:-200%; left:40%;
+			}
+			#divFigura{
+				position:absolute; top:23%; left:30%;
+				width:210px;	height:210px;
+				border-radius:0%;
+				background: #6bebcf; /* For browsers that do not support gradients */
+				transition: visibility 0.3s, border-radius 0.8s, transform 0.8s;
+				-webkit-transform: rotate(45deg);
+				-ms-transform: rotate(45deg);
+				transform: rotate(45deg);
+			}
+			.trianguloDerecha{
+				position:absolute; top:33%; left: 17%;
+				width:0 !important;	height:0 !important;
+				border-top:123px solid transparent !important;
+				border-bottom:123px solid transparent !important;
+				border-left:247px solid #6bebcf !important;
+				transition: visibility 0.3s, transform 0.8s;
+			}
+		}
+		@media (min-width: 1300px){
+			.acomodaLigas{
+				position: absolute; top:-220%; left:40%;
+			}
+			#divFigura{
+				position:absolute; top:23%; left:28%;
+				width:240px;	height:240px;
+				border-radius:0%;
+				background: #6bebcf; /* For browsers that do not support gradients */
+				transition: visibility 0.3s, border-radius 0.8s, transform 0.8s;
+				-webkit-transform: rotate(45deg);
+				-ms-transform: rotate(45deg);
+				transform: rotate(45deg);
+			}
+			.trianguloDerecha{
+				position:absolute; top:32%; left: 17%;
+				width:0 !important;	height:0 !important;
+				border-top:130px solid transparent !important;
+				border-bottom:130px solid transparent !important;
+				border-left:260px solid #6bebcf !important;
+				transition: visibility 0.3s, transform 0.8s;
+			}
+		}
+		@media (min-width: 1500px){
+			.acomodaLigas{
+				position: absolute; top:-270%; left:40%;
+			}
+			#divFigura{
+				position:absolute; top:25%; left:27%;
+				width:260px;	height:260px;
+				border-radius:0%;
+				background: #6bebcf; /* For browsers that do not support gradients */
+				transition: visibility 0.3s, border-radius 0.8s, transform 0.8s;
+				-webkit-transform: rotate(45deg);
+				-ms-transform: rotate(45deg);
+				transform: rotate(45deg);
+			}
+			.trianguloDerecha{
+				position:absolute; top:30%; left: 15%;
+				width:0 !important;	height:0 !important;
+				border-top:153px solid transparent !important;
+				border-bottom:153px solid transparent !important;
+				border-left:306px solid #6bebcf !important;
+				transition: visibility 0.3s, transform 0.8s;
+			}
+		}
+		.altura50Celda{
+			height:50px;
 		}
 	</style>
 	<!--	********* Incluir Jquery	***************	-->
@@ -102,12 +202,14 @@
 		function tranformaFigura(seccion){
 			switch(seccion){
 				case 1:	//circulo
-					$('#imgApartadoEdificio').fadeOut();
-					setTimeout(function(){
-					$('#imgApartadoEdificio').attr('src','imagenes/ventana/conocenos/slider/alineacionProyecto.png');
-						$('#imgApartadoEdificio').addClass('animate fadeInUp');
-						$('#imgApartadoEdificio').fadeIn();
-					},400);
+					if(actual!=1){
+						$('#imgApartadoEdificio').fadeOut();
+						setTimeout(function(){
+						$('#imgApartadoEdificio').attr('src','imagenes/ventana/conocenos/slider/alineacionProyecto.png');
+							$('#imgApartadoEdificio').addClass('animate fadeInUp');
+							$('#imgApartadoEdificio').fadeIn();
+						},400);
+					}
 					if(actual==2){
 						$('#divFigura').removeClass('opaco');
 						$('#divFigura').css({transform:'rotate(-135deg)'});
@@ -126,12 +228,14 @@
 					actual =1;
 					break;
 				case 2:	//cuadrado
-					$('#imgApartadoEdificio').fadeOut();
-					setTimeout(function(){
-						$('#imgApartadoEdificio').attr('src','imagenes/ventana/conocenos/slider/antecedentes.png');
-						$('#imgApartadoEdificio').addClass('animate fadeInUp');
-						$('#imgApartadoEdificio').fadeIn();
-					},400);
+					if(actual!=2){
+						$('#imgApartadoEdificio').fadeOut();
+						setTimeout(function(){
+							$('#imgApartadoEdificio').attr('src','imagenes/ventana/conocenos/slider/antecedentes.png');
+							$('#imgApartadoEdificio').addClass('animate fadeInUp');
+							$('#imgApartadoEdificio').fadeIn();
+						},400);
+					}
 					if(actual==1){
 						$('#divFigura').removeClass('opaco');
 						$('#divFigura').css({transform:'rotate(45deg)'});
@@ -150,12 +254,14 @@
 					actual =2;
 					break;
 				case 3:	//triangulo
-					$('#imgApartadoEdificio').fadeOut();
-					setTimeout(function(){
-					$('#imgApartadoEdificio').attr('src','imagenes/ventana/conocenos/slider/alineacionProyecto.png');
-						$('#imgApartadoEdificio').addClass('animate fadeInUp');
-						$('#imgApartadoEdificio').fadeIn();
-					},400);
+					if(actual!=3){
+						$('#imgApartadoEdificio').fadeOut();
+						setTimeout(function(){
+						$('#imgApartadoEdificio').attr('src','imagenes/ventana/conocenos/slider/alineacionProyecto.png');
+							$('#imgApartadoEdificio').addClass('animate fadeInUp');
+							$('#imgApartadoEdificio').fadeIn();
+						},400);
+					}
 					if(actual==1 || actual==2){
 						$('#divFigura').removeClass('opaco');
 						$('#divTriangulo').removeClass('opaco');
