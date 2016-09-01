55889245e478da632d82cbb163d6a17e49ba5f1e
crapaud <renecrapaud@gmail.com>
2016-09-01 13:31:23 -0500

    Ventana educativa, conocenos. Integrados los apartados ligas, cubos, noticias, mapa y pie. Solución de conflictos de scripts en integración.

--- resources/views/viewConocenos/cubos.blade.php
+++ resources/views/viewConocenos/cubos.blade.php
@@ -1,9 +1,9 @@
 <!--script  src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script-->
-<script  src="js/conocenos/jquery.imagecube.js"></script>
+<!--script  src="js/conocenos/jquery.imagecube.js"></script-->
 <!--script src="js/conocenos/jquery.plugin.js"></script-->
 <script>
     $(function () {
-        $('#defaultCube').imagecube();
+       // $('#defaultCube').imagecube();
     });
 </script>
 <div class="container">
