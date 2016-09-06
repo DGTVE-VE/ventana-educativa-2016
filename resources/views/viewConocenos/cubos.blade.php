14a79b3a7df2b46592cae11dc5983499cff43637
crapaud <renecrapaud@gmail.com>
2016-09-06 17:36:59 -0500

    Conocenos. Apartado comunidad educativa y links. Agregadas clases de acomodo de elementos en resoluciones pequeñas.

--- resources/views/viewConocenos/cubos.blade.php
+++ resources/views/viewConocenos/cubos.blade.php
@@ -8,6 +8,11 @@
 	.medidaCubo{
 		width: 101%; height: 250px; z-index:1;
 	}
+	@media(max-width:991px){
+		.medidaCubo{
+			height: 150px;
+		}
+	}
 	.medidaFrente{
 		width: 100%;
 		position: absolute;
@@ -23,10 +28,10 @@
 </style>
     <div class="row">
 	
-		<div class="col-sm-12 col-md-12 col-lg-12 contenedorMapa">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 contenedorMapa">
 			<div class="text-center"><h4>COMUNIDAD EDUCATIVA</h4></div>
 		</div>
-		<div class="col-md-4 col-lg-4 padLado">
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 padLado">
 			<div id="Cubo1" class="medidaCubo">
 				<img src="imagenes/conocenos/cubos/docentesComp.png" alt="" title="docentes1"/>
 				<img src="imagenes/conocenos/cubos/docentesComp.png" alt="" title="docentes2"/>
@@ -34,7 +39,7 @@
 			<img id="frenteDocentes" src="imagenes/conocenos/cubos/docentesFrente.png" class="medidaFrente img-responsive" alt="" title="docentesFrente"/>
 			<img id="eventoDocentes" src="imagenes/conocenos/cubos/frenteEvento.png" class="detectaEvento img-responsive" alt="" title="docentesFrente"/>
 		</div>
-		<div class="col-md-4 col-lg-4 padLado">
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 padLado">
 			<div id="Cubo2" class="medidaCubo">
 				<img src="imagenes/conocenos/cubos/estudiantesComp.png" alt="" title="estudiantes1"/>
 				<img src="imagenes/conocenos/cubos/estudiantesComp.png" alt="" title="estudiantes2"/>
@@ -42,7 +47,7 @@
 			<img id="frenteEstudiantes" src="imagenes/conocenos/cubos/estudiantesFrente.png" alt="" class="medidaFrente img-responsive" title="estudiantesFrente"/>
 			<img id="eventoEstudiantes" src="imagenes/conocenos/cubos/frenteEvento.png" alt="" class="detectaEvento img-responsive" title="estudiantesFrente"/>
 		</div>
-		<div class="col-md-4 col-lg-4 padLado">
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 padLado">
 			<div id="Cubo3" class="medidaCubo">
 				<img src="imagenes/conocenos/cubos/padresComp.png" alt="" title="padres1"/>
 				<img src="imagenes/conocenos/cubos/padresComp.png" alt="" title="padres2"/>
--- resources/views/viewConocenos/links.blade.php
+++ resources/views/viewConocenos/links.blade.php
@@ -2,29 +2,29 @@
     <div class="col-sm-12 col-md-12 col-lg-12 contenedorMapa">
         <div class="text-center"><h4>LINKS</h4></div>
     </div>
-    <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+    <div class="col-xs-12 col-sm-12 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
         <section class="slider">
             <div class="flexslider">
                 <ul class="slides">
                     <li>
-                        <div class="col-md-4">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <a href="http://www.gob.mx/amexcid" target="_blank"><img src="imagenes/ventana/conocenos/links/amexcid.png" alt="..." class="img-responsive"></a>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <a href="http://www.canal6.com.ni/" target="_blank"><img src="imagenes/ventana/conocenos/links/canal6.png" alt="..." class="img-responsive"></a>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <a href="http://www.csuca.org/" target="_blank"><img src="imagenes/ventana/conocenos/links/csuca.png" alt="..." class="img-responsive"></a>
                         </div>
                     </li>
                     <li>
-                        <div class="col-md-4">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <a href=" http://www.sica.int/cecc/" target="_blank"><img src="imagenes/ventana/conocenos/links/sica.png" alt="..." class="img-responsive"></a>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <a href="http://www.fundacioncatolica.org/tv_educativa.html" target="_blank"><img src="imagenes/ventana/conocenos/links/suyapa.png" alt="..." class="img-responsive"></a>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <a href="http://www.uv.mx/" target="_blank"><img src="imagenes/ventana/conocenos/links/UVeracruzana.png" alt="..." class="img-responsive"></a>
                         </div>
                     </li>
