58b67bcd687819d7897ef8c5c9139f11ffbbb68f
crapaud <renecrapaud@gmail.com>
2016-09-08 12:51:07 -0500

    Conocenos. Apartado noticias. Agregados los archivos pdf de las noticias que se mostraran inicialmente. Agregadas las ventanas modales que mostrarán los archivos al presionar el botón ver más de cada noticia.

--- resources/views/viewConocenos/noticias.blade.php
+++ resources/views/viewConocenos/noticias.blade.php
@@ -34,21 +34,21 @@
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 paddAbajo">
                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 padDiv10px">
                     {{HTML::image('imagenes/conocenos/noticia1.jpg','Noticia Conocenos',array( 'class'=>'imgNoticia img-responsive'))}}
-                    <div class="posicionVerMas"><a data-toggle="modal" href=""><div class="text-center vermas">Ver más</div></a></div>
+                    <div class="posicionVerMas"><a data-toggle="modal" data-target="#modalNoticia1" href="#"><div class="text-center vermas">Ver más</div></a></div>
                 </div>
                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 padDiv10px">
                     {{HTML::image('imagenes/conocenos/noticia2.jpg','Noticia Conocenos',array( 'class'=>'imgNoticia img-responsive'))}}
-                    <div class="posicionVerMas"><a><div class="text-center vermas">Ver más</div></a></div>
+                    <div class="posicionVerMas"><a data-toggle="modal" data-target="#modalNoticia2" href="#"><div class="text-center vermas">Ver más</div></a></div>
                 </div>                
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 padDiv10px">
                     {{HTML::image('imagenes/conocenos/noticia4.jpg','Noticia Conocenos',array( 'class'=>'imgNoticia img-responsive'))}}
-                    <div class="posicionVerMas"><a><div class="text-center vermas">Ver más</div></a></div>
+                    <div class="posicionVerMas"><a data-toggle="modal" data-target="#modalNoticia3" href="#"><div class="text-center vermas">Ver más</div></a></div>
                 </div>
                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 padDiv10px">
                     {{HTML::image('imagenes/conocenos/noticia3.jpg','Noticia Conocenos',array( 'class'=>'imgNoticia img-responsive'))}}
-                    <div class="posicionVerMas"><a><div class="text-center vermas">Ver más</div></a></div>
+                    <div class="posicionVerMas"><a data-toggle="modal" data-target="#modalNoticia4" href="#"><div class="text-center vermas">Ver más</div></a></div>
                 </div>                
             </div>
         </div>
@@ -67,22 +67,70 @@
 
 
 <!--Modales de Noticias--!>
-<!--Modal para Noticia 1-->
-<!-- Ventana modal para calendario escolar -->
-<div class="modal fade" id="modalNoticia1" role="dialog">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="panel-body">
-                    <!--<embed width="100%" height="500px" src="{{url('pdf/mediateca/calendario_185.pdf')}}" type="application/pdf"></embed>-->
-                </div>
-            </div>
-        </div>
-    </div>
-</div> <!-- Fin modal calendario escolar--> <!-- Fin modal de Noticia 1-->
+	<!--Modal para Noticia 1-->
+	<div class="modal fade" id="modalNoticia1" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						lectura en la era móvil
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>
+					<div class="panel-body">
+						<embed width="100%" height="500px" src="{{url('pdf/conocenos/lecturaEraMovil.pdf')}}" type="application/pdf"></embed>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Modal para Noticia 2-->
+	<div class="modal fade" id="modalNoticia2" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Students, computers & learning
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>
+					<div class="panel-body">
+						<embed width="100%" height="500px" src="{{url('pdf/conocenos/computersLearningInfographic.pdf')}}" type="application/pdf"></embed>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Modal para Noticia 3-->
+	<div class="modal fade" id="modalNoticia3" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						eLAC Plan de Accion
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>
+					<div class="panel-body">
+						<embed width="100%" height="500px" src="{{url('pdf/conocenos/eLAC_Plan_de_Accion.pdf')}}" type="application/pdf"></embed>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Modal para Noticia 4-->
+	<div class="modal fade" id="modalNoticia4" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Objetivos de desarrollo
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>
+					<div class="panel-body">
+						<embed width="100%" height="500px" src="{{url('pdf/conocenos/mdg-report-2015.pdf')}}" type="application/pdf"></embed>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 <!--Modales de Noticias--!>
 
 <!--Facebook-->
