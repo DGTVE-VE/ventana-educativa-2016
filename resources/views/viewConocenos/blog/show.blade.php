d226e794f4a25044366ff6eefd73fb9bc3d83628
Crapaud <renecrapaud@gmail.com>
2016-10-26 17:26:49 -0500

    Blog conocenos. Ajuste de estilos de contenido principal, menú de ventana y barra lateral, para aproximar a visualización deseada, para la vista principal de entradas de blog y para vista de contenido de cada tema del blog.

--- resources/views/viewConocenos/blog/show.blade.php
+++ resources/views/viewConocenos/blog/show.blade.php
@@ -5,140 +5,151 @@ Ventana Educativa / Conocenos
 @section('menuConocenos')
 @include('viewVentana.encabezadoVentana')
 <script>
-	$('#imagenLogoVentana').attr('src','{{url("imagenes/ventana/encabezado/logoNegro/logoventana.png")}}');
-	$('#iconoManuApps').attr('src','{{url("imagenes/ventana/encabezado/logoNegro/iconoApps.png")}}');
+	$('#navegacionVentana').removeClass('navbar-default');
+	$('#navegacionVentana').removeClass('menuVentana');
+	$('#navegacionVentana').addClass('navbar-inverse');
 </script>
 @endsection
 @section('cuerpoConocenos')
+<style>
+	.fondoBlog{
+		background-image: url('/ventana-educativa-2016/public/imagenes/conocenos/blog/backBlog.jpg');
+	}
+	.fondoPanelIzq{
+		background-color: #545454;
+		min-height:100%;
+		color: white;
+	}
+	h4 > a{
+		color: white;
+	}
+	h4 > a:visited{
+		color: white;
+	}
+	.fondoBlanco{
+		background-color: white;
+		-webkit-box-shadow: 9px 9px 0px -5px rgba(0,0,0,1);
+		-moz-box-shadow: 9px 9px 0px -5px rgba(0,0,0,1);
+		box-shadow: 9px 9px 0px -5px rgba(0,0,0,1);
+	}
+</style>
 <div class="menuBlog"> 
     <a href="{{url('conocenos')}}" class="text-center">
-        <h4 class="glyphicon glyphicon-home" style="color: black;"></h4><br/>
+        <h4 class="glyphicon glyphicon-home" style="color: white;"></h4><br/>
     </a>
 </div>
-<div class="row" style="position:relative; top:50px;">
-    <div class='col-md-2'></div>
-    <div class='col-md-5 txtBlogRed'>
-        <br>
-        <div class="col-md-12"><img src="{{url ($blog->imagen)}}" class="img-responsive center-block"></div>                
-        <h3 class="text-center"> {{$blog->titulo}}</h3>
-        <?php
-        $dt = new DateTime($blog->created_at);
-        $date = $dt->format('m/d/Y');
-        ?>
-        {{$date}}
-        <h4 class="text-left">Por: {{$colaborador->user->name}}</h4>
-        <div class="text-justify">
-            {!!$blog->cuerpo!!}
-        </div>
-        <h3>Conversación de expertos </h3>
-        @if(Auth::check())
-        @if (Auth::user ()->is_researcher)
-        <form action="{{url ('conocenos/blog/comment')}}" method="POST" enctype="multipart/form-data">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}" />    
-            <input type="hidden" name="id_colaborador" value="{{$colaborador->id}}" />    
-            <input type="hidden" name="id_blog" value="{{$blog->id}}" />    
-            <input type="hidden" name="id_comment" value="0" />            
-            <textarea name="comment"></textarea>
-            <button type="submit" class="btn btn-default"> Comentar </button>
-        </form>
-        @endif
-        @endif
-        <div>
-            @foreach ($blog->comments as $comment)
-            @if ($comment->id_comment == 0) 
-            <div class="fondo_Claro text-justify">
-                @include('viewConocenos.blog.comments', ['comment'=>$comment])  
-            </div>
-            <hr>
-            @endif
-            @endforeach 
-        </div>
+<div class="row fondoBlog">
+    <div class='col-md-6 col-md-offset-1 txtBlogRed'>
+		<div class="col-md-12" style="padding:50px;"></div>
+		<div class="col-md-12 fondoBlanco">
+			<div class="col-md-12"><img src="{{url ($blog->imagen)}}" class="img-responsive center-block"></div>                
+			<h3 class="text-center"> {{$blog->titulo}}</h3>
+			<?php
+			$dt = new DateTime($blog->created_at);
+			$date = $dt->format('m/d/Y');
+			?>
+			{{$date}}
+			<h4 class="text-left">Por: {{$colaborador->user->name}}</h4>
+			<div class="text-justify">
+				{!!$blog->cuerpo!!}
+			</div>
+			<h3>Conversación de expertos </h3>
+			@if(Auth::check())
+			@if (Auth::user ()->is_researcher)
+			<form action="{{url ('conocenos/blog/comment')}}" method="POST" enctype="multipart/form-data">
+				<input type="hidden" name="_token" value="{{ csrf_token() }}" />    
+				<input type="hidden" name="id_colaborador" value="{{$colaborador->id}}" />    
+				<input type="hidden" name="id_blog" value="{{$blog->id}}" />    
+				<input type="hidden" name="id_comment" value="0" />            
+				<textarea name="comment"></textarea>
+				<button type="submit" class="btn btn-default"> Comentar </button>
+			</form>
+			@endif
+			@endif
+			<div>
+				@foreach ($blog->comments as $comment)
+				@if ($comment->id_comment == 0) 
+				<div class="fondo_Claro text-justify">
+					@include('viewConocenos.blog.comments', ['comment'=>$comment])  
+				</div>
+				<hr>
+				@endif
+				@endforeach 
+			</div>
+		</div>
     </div>
-    <div class='col-md-4 txtBlogRed'>
-        <br>
-        <!--Acerca de-->
-        <div class="panel-heading blogPanel">
-            <h4 class="panel-title">
-                <a data-toggle="collapse" href="#acercade">
-                    Acerca del autor
-                    <i class="fa fa-plus pull-right" aria-hidden="true"></i>
-                </a>
-            </h4>
-        </div>
-        <div id="acercade" class="panel-collapse collapse">
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-md-2"><img src="{{asset($colaborador->url_foto)}}" width="50" height="50"></div>
-                    <div class="col-md-10"><h5>{{$colaborador->user->name}}</h5></div>
-                    <div class="col-md-10"><h5>{{$colaborador->puesto}}</h5></div>
-                    <div class="col-md-12"><p class="text-justify">{{$colaborador->resena}}</p></div>
-                </div>
-            </div>
-        </div>
-        <div>
-            <br>
-            <div class="panel-group bordeBlog"><br>
-                <div class="panel panel-default">
-                    <div class="panel-heading blogPanel">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" href="#leidos">
-                                Más leído
-                                <i class="fa fa-plus pull-right" aria-hidden="true"></i>                            
-                            </a>
-                        </h4>
-                    </div>                    
-                    <div id="leidos" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            <ol>
-                                @foreach ($leidos as $leido)
-                                <li> <a href="{{url("conocenos/blog/$leido->id")}}"> {{$leido->titulo}}</a></li>                    
-                                @endforeach                        
-                            </ol>
-                        </div>
-                    </div>
-                    <br>
-                    <!--recientes-->
-                    <div class="panel-heading blogPanel">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" href="#recientes">
-                                Más reciente
-                                <i class="fa fa-plus pull-right" aria-hidden="true"></i>                            
-                            </a>
-                        </h4>
-                    </div>
-                    <div id="recientes" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            <ol>
-                                @foreach ($recientes as $reciente)
-                                <li> <a href="{{url("conocenos/blog/$reciente->id")}}"> {{$reciente->titulo}}</a></li>
-                                @endforeach 
-                            </ol>
-                        </div>
-                    </div>
-                    <br>
-                    <!--comentados-->
-                    <div class="panel-heading blogPanel">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" href="#comentados">
-                                Más comentado
-                                <i class="fa fa-plus pull-right" aria-hidden="true"></i>
-                            </a>
-                        </h4>
-                    </div>
-                    <div id="comentados" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            <ol>
-                                @foreach ($comentados as $comentado)
-                                <li> <a href="{{url("conocenos/blog/$comentado->id")}}"> {{$comentado->titulo}}</a></li>
-                                @endforeach                       
-                            </ol>
-                        </div>
-                    </div>                   
-                </div>
-            </div>
-            <br>
-        </div>
-    </div>
+	<div class="col-md-4 col-md-offset-1 fondoPanelIzq">
+		<div class='col-md-12' style="padding:50px;"></div>
+		<div class="col-md-10 col-md-offset-1">
+			<div class="col-md-12">
+				<h3><em>  Acerca del autor </em></h3>
+			</div>
+			<div class="col-md-12">
+				<div class="col-md-2"><img src="{{asset($colaborador->url_foto)}}" width="50" height="50"></div>
+				<div class="col-md-10"><h5>{{$colaborador->user->name}}</h5></div>
+				<div class="col-md-10"><h5>{{$colaborador->puesto}}</h5></div>
+				<div class="col-md-12"><p class="text-justify">{{$colaborador->resena}}</p></div>
+			</div>
+			<div class="col-md-12">
+				<h3><em> lo más leído </em></h3>
+			</div>
+			{{--*/ $i=1; /*--}}
+			@foreach ($leidos as $leido)
+			<div class="col-md-2">
+				<h1> <strong>{{ $i++ }} </strong></h1>
+			</div>
+			<div class="col-md-10">
+				<h4> <a href="{{url('conocenos/blog/$leido->id')}}"> {{$leido->titulo}}</a> </h4>
+			</div>
+			<div class="col-md-12">
+			</div>
+			@endforeach
+			<div class="col-md-12">
+				<h3> <em>lo más reciente </em></h3>
+			</div>
+			{{--*/ $i=1; /*--}}
+			@foreach ($recientes as $reciente)
+			<div class="col-md-2">
+				<h1> <strong>{{ $i++ }} </strong></h1>
+			</div>
+			<div class="col-md-10">
+				<h4> <a href="{{url('conocenos/blog/$reciente->id')}}"> {{$reciente->titulo}}</a> </h4>
+			</div>
+			<div class="col-md-12">
+			</div>
+			@endforeach
+			<div class="col-md-12">
+				<h3> <em>lo más comentado </em></h3>
+			</div>
+			{{--*/ $i=1; /*--}}
+			@foreach ($comentados as $comentado)
+			<div class="col-md-2">
+				<h1><strong> {{ $i++ }} </strong></h1>
+			</div>
+			<div class="col-md-10">
+				<h4> <a href="{{url('conocenos/blog/$comentado->id')}}"> {{$comentado->titulo}}</a> </h4>
+			</div>
+			<div class="col-md-12">
+			</div>
+			@endforeach
+			
+		</div>
+		<div class="col-md-12">
+		</div>
+		@if (Auth::guest ())
+		@else
+		@if (Auth::user()->is_researcher)
+		<!--Agregar publicación-->
+		<div class="panel-heading blogPanel col-md-10 col-md-offset-1 text-center">
+			<h4 class="panel-title">
+				<a href="{{url('conocenos/blog/create')}}">
+					Agregar publicación
+				</a>
+			</h4>
+		</div>
+		@endif
+		@endif 
+	</div>
 </div>
 @include('viewConocenos.pie')
 <script>
