c75aee3b707e873768ea5f08fc45ee27970d6365
Crapaud <renecrapaud@gmail.com>
2016-11-10 11:32:28 -0600

    Conocenos. Menú principal ventana. Ajuste de posición de iconos de apps y registro.

--- resources/views/viewConocenos/encabezado.blade.php
+++ resources/views/viewConocenos/encabezado.blade.php
@@ -5,6 +5,13 @@
 	$('#navegacionVentana').css('margin-bottom','-15px');
 	$('#navegacionVentana').addClass('navbar-fixed-top');
 	$('#cuerpoPanel').css('padding-top','0');
-
+	$('.navbar-right').css('margin-right','0');
+	$('#imagenLogoVentana').css('z-index','0');
+	$('#buttonColapse').css('z-index','1');
+	$('.backNegro').addClass('backGris');
+	$('.backNegro').removeClass('backNegro');
+	$('.bordeBoton').addClass('bordeGris');
+	$('.bordeBoton').removeClass('bordeBoton');
+	
 </script>
 	<!--	**************************************	-->
