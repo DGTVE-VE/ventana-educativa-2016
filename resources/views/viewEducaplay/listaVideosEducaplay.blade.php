0470e33a8d58200aa4d749d43e736a660cc42d3d
Luis Sassón <luisasson2911@gmail.com>
2016-11-17 15:55:53 -0600

    Educaplay compartir facebook

--- resources/views/viewEducaplay/listaVideosEducaplay.blade.php
+++ resources/views/viewEducaplay/listaVideosEducaplay.blade.php
@@ -10,6 +10,47 @@ Educaplay
 @if($episodiosSerie!=null)
 	<script src="{{asset ('js/bootstrap-rating-input.min.js')}}"></script>
 	<script src="https://www.youtube.com/player_api"></script>
+        <!--El siguiente fragmento de codigo es para el uso de Facebook en la aplicación-->
+	<script>
+		window.fbAsyncInit = function () {
+		FB.init({
+		appId: '1408909052733113',
+		xfbml: true,
+		version: 'v2.6'
+		});
+		};
+
+		(function (d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {
+		return;
+		}
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+    </script>
+    <!--fin codigo facebook-->
+     <!--sdk twitter-->
+    <script>window.twttr = (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0],
+                    t = window.twttr || {};
+            if (d.getElementById(id))
+                return t;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+
+            t._e = [];
+            t.ready = function (f) {
+                t._e.push(f);
+            };
+
+            return t;
+        }(document, "script", "twitter-wjs"));</script>
+    <!--fin sdk twitter-->
 	<script src="{{url('js/educaplay/listaVideos.js')}}"></script>
 	<script>
 		//		--------------		En video terminado o pausado guarda tiempo transcurrido
@@ -100,19 +141,38 @@ Educaplay
 		@else
 		<div id="episodio8" class="row" style="color:white;">
 			<input type="hidden" id="VideoReproduce" value="{{EducaplayController::consultaRatingXURL($urlVideo)}}"/>
-			@endif
+			@endif                        
 			<div class="col-md-5 col-md-offset-1 text-right">
 				<div id="player" class="col-xs-12 col-sm-12 col-md-12 col-lg-12" align="center">    </div>
 				@if(Auth::check ())
 				<div id="divRating">
 					<input type="number" name="rating" id="star-rating" class="" data-icon-lib="fa" data-active-icon="fa-star" data-inactive-icon="fa-star-o" onchange="guardaRating(this.value)"/>
 				</div>
+                                <!--twitter-->
+                                <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a> <script>!function (d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                                    if (!d.getElementById(id)) {
+                                        js = d.createElement(s);
+                                        js.id = id;
+                                        js.src = p + '://platform.twitter.com/widgets.js';
+                                        fjs.parentNode.insertBefore(js, fjs);
+                                    }
+                                }(document, 'script', 'twitter-wjs');</script>
+                                <!--facebook-->
+                                <div 
+                                class="fb-share-button" 
+                                data-layout="button" 
+                                data-mobile-iframe="true"
+                                >
+                            </div>
 				<script>
 					var tmpRating = $('#VideoReproduce').val();
 					refrescaRating(tmpRating);
 				</script>
+                                
 				@endif
 			</div>
+                        
 			<div class="col-md-4 col-md-offset-1">
 				{{--*/ $infoVideo = EducaplayController::consultaDatosVideo($idVideo); /*--}}
 				@if($infoVideo==null)
@@ -145,6 +205,7 @@ Educaplay
 					<span class="estiloTxt">{{substr($serie->sinopsis,0,101)."....."}}</span><br>
 					@endif
 				</div>
+                                <!--<h4>Hola a todos</h4>-->
 				<input type="hidden" id="temporadaSerie{{$serie->id}}" value="{{$serie->temporada}}" />
 				<input type="hidden" id="episodioSerie{{$serie->id}}" value="{{$serie->capitulo}}" />
 				<input type="hidden" id="sinopsisSerie{{$serie->id}}" value="{{$serie->sinopsis}}" />
@@ -171,5 +232,21 @@ Educaplay
 		<p style="color:white; font-size:2em;"> P R Ó X I M A M E N T E </p>
 	</div>
 @endif
+
+        <!--metadatos para compartir en facebook-->
+	<meta property="og:url" content="http://ventana.televisioneducativa.gob.mx/{{Request::path()}}" /> 
+	<meta property="fb:app_id" content="1408909052733113" /> 
+	<meta property="og:type" content="article" />                                  
+	<meta property="og:title" content="{{$serie->titulo_serie}}" /> 
+	<meta property="og:image" content="" />                                             
+	<meta property="og:description" content="" />
+
+	<!--twitter metas-->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@tveducativamx">
+	<meta name="twitter:creator" content="@SarahMaslinNir">
+	<meta name="twitter:title" content="">
+	<meta name="twitter:description" content="">
+	<meta name="twitter:image" content="">
 
 @endsection
