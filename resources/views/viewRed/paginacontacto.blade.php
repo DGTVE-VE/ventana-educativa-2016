d774b16f07ffa9a9c600ccc37d61f44310cecf93
Crapaud <renecrapaud@gmail.com>
2017-07-20 14:04:13 -0500

    RedMite. Sección comentarios. Corregir error de página no encontrada al envíar comentarios.

--- resources/views/viewRed/paginacontacto.blade.php
+++ resources/views/viewRed/paginacontacto.blade.php
@@ -33,7 +33,7 @@ Contacto
     <div class="panel-body col-md-12">
         <div class="row col-sm-5 col-md-offset-4">
             <div><h4>Envíanos tus dudas, comentarios o sugerencias.</h4></div>
-            <form action="guardaContactoConocenos" method="post">
+            <form action="guardaContacto" method="post">
                 {!!csrf_field()!!}
                 <div class="input-group">
                     <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
