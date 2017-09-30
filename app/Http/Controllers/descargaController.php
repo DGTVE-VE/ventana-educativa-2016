4cb1ca98ea869972338af744f2a4e5c36d981c65
Crapaud <renecrapaud@gmail.com>
2017-09-30 00:02:57 -0500

    prueba generar liga descarga

--- app/Http/Controllers/descargaController.php
+++ app/Http/Controllers/descargaController.php
@@ -30,7 +30,7 @@ $ch = curl_init($url);
 $fp = fopen($localfile,'w');
 
 curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
+//curl_setopt($ch, CURLOPT_HEADER, 0);
 
 curl_exec($ch);
 curl_close($ch);
