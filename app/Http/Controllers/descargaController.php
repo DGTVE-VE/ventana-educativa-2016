402a9bb67ee0796fa66daad7eeb66ba94ae6d4bc
Crapaud <renecrapaud@gmail.com>
2017-09-29 22:15:20 -0500

    EducaMedia. Vista de videos. Corrección al generar liga de descarga del video.

--- app/Http/Controllers/descargaController.php
+++ app/Http/Controllers/descargaController.php
@@ -22,29 +22,33 @@ class descargaController extends Controller {
         $contexto = stream_context_create($opciones);
         if(file_get_contents('http://www.youtube.com/get_video_info?video_id='.$idVideo, false, $contexto)){
             parse_str(file_get_contents('http://www.youtube.com/get_video_info?video_id='.$idVideo, false, $contexto), $video_data);
-        }
-        $streams = $video_data['url_encoded_fmt_stream_map'];
-        $streams = explode(',',$streams);
-        $counter = 1;
-        foreach ($streams as $streamdata) {
-           //printf("Stream %d:<br/>----------------<br/><br/>", $counter);
+            $streams = $video_data['url_encoded_fmt_stream_map'];
+            $streams = explode(',',$streams);
+            $counter = 1;
+            foreach ($streams as $streamdata) {
+               //printf("Stream %d:<br/>----------------<br/><br/>", $counter);
 
-           parse_str($streamdata,$streamdata);
-           foreach ($streamdata as $key => $value) {
-                   if ($key == "url") {
-                           $value = urldecode($value);
-                           //printf("%s", $value);
-                           return $value;
-                   } else {
-                           //printf("%s", $value);
-                           //return $value;
-                   }
-           }
-           break;
-           //$counter = $counter+1;
-           //printf("<br/><br/>");
-           
+               parse_str($streamdata,$streamdata);
+               foreach ($streamdata as $key => $value) {
+                       if ($key == "url") {
+                               $value = urldecode($value);
+                               //printf("%s", $value);
+                               return $value;
+                       } else {
+                               //printf("%s", $value);
+                               //return $value;
+                       }
+               }
+               break;
+               //$counter = $counter+1;
+               //printf("<br/><br/>");
+               
+            }
         }
+        else{
+            return $value=null;
+        }
+
         // start server and go to http://url/?id=video-id
     }
 }
