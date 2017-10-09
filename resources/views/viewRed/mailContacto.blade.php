d7ace5228d1ff959af39f643ae9923d39df24b1f
Crapaud <renecrapaud@gmail.com>
2017-10-09 12:49:33 -0500

    REDMITE. Ajuste en plantilla de correo de mensaje en sección contacto.

--- resources/views/viewRed/mailContacto.blade.php
+++ resources/views/viewRed/mailContacto.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <body>
-        <img src="{{asset('imagenes/red/LogoRed/logoredmesoamericana.png')}}"/><h2>REDMITE. Recepción de comentarios.</h2>
+        <h2><img src="{{asset('imagenes/red/LogoRed/logoredmesoamericana.png')}}" alt="REDMITE"/> Recepción de comentarios.</h2>
         <p> Ha sido recibido un mensaje desde la sección Contacto de REDMITE, con la siguiente información: </p>
         <p> Nombre: {{$contacto->nombre_contacto}} <br>
             Correo eletcrónico: {{$contacto->correo}} <br>
