db3f3e0b8fa5bc0e11f2c9603075fcb8048abc9a
Crapaud <renecrapaud@gmail.com>
2017-10-18 16:48:11 -0500

    Pruebas de código en node para descarga de videos de YT

--- public/js/mediateca/pruebaNode.js
+++ public/js/mediateca/pruebaNode.js
@@ -1,7 +1,14 @@
 var http = require('http');
+    const fs = require('fs');
+    const ytdl = require('ytdl-core');
 
+    ytdl('https://youtu.be/g4f1vvXUBYQ')
+            //.pipe(fs.createWriteStream('C:\\xampp\\htdocs\\ventanaE\\public\\g4f1vvXUBYQ.mp4'));
+            .pipe(fs.createWriteStream('g4f1vvXUBYQ.mp4'));
+            
+    console.log('ejecutado');
  
-http.createServer(function (req, res) {
+/*http.createServer(function (req, res) {
     
     const fs = require('fs');
     const ytdl = require('ytdl-core');
@@ -13,4 +20,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Mensaje Prueba Descarga!');
     
-}).listen(8085);
+}).listen(8085);*/
