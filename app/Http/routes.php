c4e2a0be45f5c86926bbf650d096bdc80c1fef2c
Crapaud <renecrapaud@gmail.com>
2018-03-16 17:34:19 -0600

    Olvido de contraseña. Cambiar rutas del segmento de sesión. Por error al solicitar cambio.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,15 +24,6 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('logout', 'SessionsController@destroy');
     Route::post('cambiaAvatar', 'VentanaController@cambiaAvatar');
 
-    // Password reset link request routes...
-    Route::get('password/email', 'Auth\PasswordController@getEmail');
-    Route::post('password/email', 'Auth\PasswordController@postEmail');
-    //
-    //// Password reset routes...
-    Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
-    Route::post('password/reset', 'Auth\PasswordController@postReset');
-
-
     Route::get('verificaCorreo/{correo}/{hash}', 'VentanaController@activaCorreo');
     Route::any('registro', 'VentanaController@registro');
     Route::get('acceso', 'VentanaController@acceso');
@@ -198,6 +189,14 @@ Route::group(['middleware' => 'web'], function () {
     
 });
 
+    // Password reset link request routes...
+    Route::get('password/email', 'Auth\PasswordController@getEmail');
+    Route::post('password/email', 'Auth\PasswordController@postEmail');
+    //
+    //// Password reset routes...
+    Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+    Route::post('password/reset', 'Auth\PasswordController@postReset');
+    
 //Route::get ('api/getImagenes/{tipo}/{id}', '');
 
 
